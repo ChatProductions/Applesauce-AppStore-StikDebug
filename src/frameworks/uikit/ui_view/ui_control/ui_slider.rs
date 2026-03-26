213a8f7d5d380002d77590f052ca80296e9affab
j92580498-max <j92580498@gmail.com>
2026-03-26 10:22:14 +0200

    Update ui_slider.rs

--- src/frameworks/uikit/ui_view/ui_control/ui_slider.rs
+++ src/frameworks/uikit/ui_view/ui_control/ui_slider.rs
@@ -6,7 +6,15 @@
 //! `UISlider`.
 
 use crate::frameworks::core_graphics::CGRect;
-use crate::objc::{id, msg_super, objc_classes, todo_objc_setter, ClassExports};
+use crate::objc::{id, msg_super, objc_classes, ClassExports, HostObject, NSZonePtr};
+
+#[derive(Default)]
+pub(super) struct UISliderHostObject {
+    pub(super) value: f32,
+    pub(super) minimum_value: f32,
+    pub(super) maximum_value: f32,
+}
+impl HostObject for UISliderHostObject {}
 
 pub const CLASSES: ClassExports = objc_classes! {
 
@@ -14,25 +22,57 @@ pub const CLASSES: ClassExports = objc_classes! {
 
 @implementation UISlider: UIControl
 
++ (id)allocWithZone:(NSZonePtr)_zone {
+    let host_object = Box::new(UISliderHostObject {
+        value: 0.5,
+        minimum_value: 0.0,
+        maximum_value: 1.0,
+    });
+    env.objc.alloc_object(this, host_object, &mut env.mem)
+}
+
 - (id)initWithFrame:(CGRect)frame {
-    log!("[(UISlider*){:?} initWithFrame:{:?}] TODO: Implement UISlider. The control won't be rendered.", this, frame);
+    log_dbg!("[(UISlider*){:?} initWithFrame:{:?}] TODO: Implement UISlider. The control won't be rendered.", this, frame);
     msg_super![env; this initWithFrame:frame]
 }
 
 // NSCoding implementation
 - (id)initWithCoder:(id)coder {
-    log!("[(UISlider*){:?} initWithCoder:{:?}] TODO: Implement UISlider. The control won't be rendered.", this, coder);
+    log_dbg!("[(UISlider*){:?} initWithCoder:{:?}] TODO: Implement UISlider. The control won't be rendered.", this, coder);
     msg_super![env; this initWithCoder:coder]
 }
 
-- (())setMinimumValueImage:(id)img { // UIImage *
-    todo_objc_setter!(this, img);
-}
-- (())setMaximumValueImage:(id)img { // UIImage *
-    todo_objc_setter!(this, img);
+- (f32)value {
+    env.objc.borrow::<UISliderHostObject>(this).value
 }
 
-// TODO: all of it
+- (())setValue:(f32)value {
+    env.objc.borrow_mut::<UISliderHostObject>(this).value = value;
+}
+
+- (f32)minimumValue {
+    env.objc.borrow::<UISliderHostObject>(this).minimum_value
+}
+
+- (())setMinimumValue:(f32)value {
+    env.objc.borrow_mut::<UISliderHostObject>(this).minimum_value = value;
+}
+
+- (f32)maximumValue {
+    env.objc.borrow::<UISliderHostObject>(this).maximum_value
+}
+
+- (())setMaximumValue:(f32)value {
+    env.objc.borrow_mut::<UISliderHostObject>(this).maximum_value = value;
+}
+
+- (())setMinimumValueImage:(id)_img { // UIImage *
+    // Просто заглушка, так как UI не рендерится
+}
+
+- (())setMaximumValueImage:(id)_img { // UIImage *
+    // Просто заглушка, так как UI не рендерится
+}
 
 @end
 
