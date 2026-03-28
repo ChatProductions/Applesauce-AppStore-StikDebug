86d65024624dd19a5935eec5a415681ff58388b0
j92580498-max <j92580498@gmail.com>
2026-03-28 15:29:36 +0200

    Update ns_locale.rs

--- src/frameworks/foundation/ns_locale.rs
+++ src/frameworks/foundation/ns_locale.rs
@@ -63,6 +63,16 @@ fn get_preferred_countries(env: &mut Environment) -> Vec<String> {
     }
 }
 
+/// Extract the language subtag from a locale identifier.
+/// Handles "ru", "ru_RU", "ru-RU", "en_US" etc.
+fn language_from_locale_identifier(identifier: &str) -> &str {
+    let sep = identifier.find('_').or_else(|| identifier.find('-'));
+    match sep {
+        Some(idx) => &identifier[..idx],
+        None => identifier,
+    }
+}
+
 struct NSLocaleHostObject {
     /// `NSString *`
     country_code: id,
@@ -85,5 +95,103 @@ pub const CLASSES: ClassExports = objc_classes! {
     env.objc.alloc_object(this, host_object, &mut env.mem)
 }
 
-// The documentation isn't clear about what the format of the strings should be,
-// but Super Monkey Ball does
+// The documentation is not clear about what the format of the strings should
+// be, but Super Monkey Ball does isEqualToString against "fr", "es", "de",
+// "it" and "ja", and its locale detection works properly, so presumably they
+// do not usually have region suffixes.
++ (id)preferredLanguages {
+    if let Some(existing) = State::get(env).preferred_languages {
+        existing
+    } else {
+        let langs = get_preferred_languages(env);
+        let lang_ns_strings = langs.into_iter().map(|lang| ns_string::from_rust_string(env, lang)).collect();
+        let new = ns_array::from_vec(env, lang_ns_strings);
+        State::get(env).preferred_languages = Some(new);
+        new
+    }
+}
+
++ (id)currentLocale {
+    if let Some(locale) = State::get(env).current_locale {
+        locale
+    } else {
+        let countries = get_preferred_countries(env);
+        let country_code = ns_string::from_rust_string(env, countries[0].clone());
+        let languages = get_preferred_languages(env);
+        let language_code = ns_string::from_rust_string(env, languages[0].clone());
+        let host_object = NSLocaleHostObject {
+            country_code,
+            language_code,
+        };
+        let new_locale = env.objc.alloc_object(
+            this,
+            Box::new(host_object),
+            &mut env.mem
+        );
+        State::get(env).current_locale = Some(new_locale);
+        new_locale
+    }
+}
+
++ (id)systemLocale {
+    if let Some(locale) = State::get(env).system_locale {
+        locale
+    } else {
+        let host_object = NSLocaleHostObject {
+            // Was confirmed on the iOS Simulator
+            country_code: nil,
+            language_code: nil,
+        };
+        let new_locale = env.objc.alloc_object(
+            this,
+            Box::new(host_object),
+            &mut env.mem
+        );
+        State::get(env).system_locale = Some(new_locale);
+        new_locale
+    }
+}
+
+// TODO: constructors, more accessors
+
+- (id)initWithLocaleIdentifier:(id)string { // NSString *
+    let str = ns_string::to_rust_string(env, string);
+    log_dbg!("[(NSLocale *){:?} initWithLocaleIdentifier:'{}']", this, str);
+    // Locale identifiers may be "ru", "ru_RU", "en-US", etc.
+    // Use a helper outside the macro to avoid closure syntax inside objc_classes!
+    let lang = language_from_locale_identifier(&str).to_string();
+    let lang_ns_string = ns_string::from_rust_string(env, lang);
+    assert!(env.objc.borrow::<NSLocaleHostObject>(this).language_code == nil);
+    env.objc.borrow_mut::<NSLocaleHostObject>(this).language_code = lang_ns_string;
+    this
+}
+
+- (())dealloc {
+    let &NSLocaleHostObject { country_code, language_code } = env.objc.borrow::<NSLocaleHostObject>(this);
+    release(env, country_code);
+    release(env, language_code);
+    env.objc.dealloc_object(this, &mut env.mem)
+}
+
+// NSCopying implementation
+- (id)copyWithZone:(NSZonePtr)_zone {
+    retain(env, this)
+}
+
+- (id)objectForKey:(id)key {
+    let key_str: &str = &ns_string::to_rust_string(env, key);
+    match key_str {
+        // Note: this is not the cleanest separation between NS and CF parts
+        // But it does work on the iOS Simulator
+        // TODO: Define NSLocaleCountryCode as kCFLocaleCountryCode
+        NSLocaleCountryCode | kCFLocaleCountryCode => {
+            let &NSLocaleHostObject { country_code, .. } = env.objc.borrow(this);
+            country_code
+        },
+        _ => unimplemented!()
+    }
+}
+
+@end
+
+};
