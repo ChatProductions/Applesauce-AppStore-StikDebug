4e6f873b83f874e1cea5fb2bb1b3aca5d77f6c39
j92580498-max <j92580498@gmail.com>
2026-03-30 10:12:56 +0300

    Update av_audio_session.rs

--- src/frameworks/av_foundation/av_audio_session.rs
+++ src/frameworks/av_foundation/av_audio_session.rs
@@ -3,7 +3,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
-//! `AudioSession.h` (Audio Session) // TODO: is this the real name?
+//! AVAudioSession (ObjC class) and AudioSession C API.
 
 use crate::abi::GuestFunction;
 use crate::dyld::{export_c_func, FunctionExports};
@@ -11,7 +11,94 @@ use crate::frameworks::carbon_core::OSStatus;
 use crate::frameworks::core_audio_types::{debug_fourcc, fourcc};
 use crate::frameworks::core_foundation::cf_run_loop::{CFRunLoopMode, CFRunLoopRef};
 use crate::mem::{guest_size_of, ConstVoidPtr, GuestUSize, MutPtr, MutVoidPtr};
+use crate::objc::{id, nil, ClassExports, HostObject, NSZonePtr};
+use crate::objc_classes;
 use crate::Environment;
+
+// ===========================================================================
+// AVAudioSession ObjC class
+// ===========================================================================
+
+struct AVAudioSessionHostObject;
+impl HostObject for AVAudioSessionHostObject {}
+
+fn new_av_audio_session(env: &mut Environment, class: id) -> id {
+    let host_object = Box::new(AVAudioSessionHostObject);
+    env.objc.alloc_object(class, host_object, &mut env.mem)
+}
+
+pub const CLASSES: ClassExports = objc_classes! {
+
+(env, this, _cmd);
+
+@implementation AVAudioSession: NSObject
+
++ (id)allocWithZone:(NSZonePtr)_zone {
+    new_av_audio_session(env, this)
+}
+
++ (id)sharedInstance {
+    log!("TODO: AVAudioSession sharedInstance");
+    new_av_audio_session(env, this)
+}
+
+- (bool)setCategory:(id)_category
+              error:(MutPtr<id>)_error {
+    log!("TODO: AVAudioSession setCategory:error:");
+    true
+}
+
+- (bool)setCategory:(id)_category
+        withOptions:(i32)_options
+              error:(MutPtr<id>)_error {
+    log!("TODO: AVAudioSession setCategory:withOptions:error:");
+    true
+}
+
+- (bool)setActive:(bool)_active
+            error:(MutPtr<id>)_error {
+    log!("TODO: AVAudioSession setActive:error:");
+    true
+}
+
+- (bool)setActive:(bool)_active
+      withOptions:(i32)_options
+            error:(MutPtr<id>)_error {
+    log!("TODO: AVAudioSession setActive:withOptions:error:");
+    true
+}
+
+- (id)category {
+    nil
+}
+
+- (())setDelegate:(id)_delegate {
+    log!("TODO: AVAudioSession setDelegate:");
+}
+
+- (f64)currentHardwareSampleRate {
+    44100.0
+}
+
+- (u32)currentHardwareOutputNumberChannels {
+    2
+}
+
+- (bool)inputIsAvailable {
+    false
+}
+
+- (f32)outputVolume {
+    1.0
+}
+
+@end
+
+};
+
+// ===========================================================================
+// AudioSession C API
+// ===========================================================================
 
 type AudioSessionInterruptionListener = GuestFunction;
 type AudioSessionPropertyListener = GuestFunction;
@@ -204,6 +291,7 @@ fn AudioSessionAddPropertyListener(
     );
     result
 }
+
 fn AudioSessionRemovePropertyListenerWithUserData(
     _env: &mut Environment,
     in_property_id: AudioSessionPropertyID,
