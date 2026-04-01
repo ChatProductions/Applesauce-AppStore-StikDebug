27b4bfc6cd4abd9c33a13a976136118d498ea583
masterfanaedoodledash-cmd <masterfanaedoodledash@gmail.com>
2026-04-01 21:11:58 +0200

    Update audio_services.rs

--- src/frameworks/audio_toolbox/audio_services.rs
+++ src/frameworks/audio_toolbox/audio_services.rs
@@ -4,37 +4,48 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 //! `AudioServices.h` (Audio Services)
-
 use crate::dyld::{export_c_func, FunctionExports};
 use crate::frameworks::carbon_core::OSStatus;
 use crate::frameworks::core_audio_types::fourcc;
-use crate::mem::{MutPtr, MutVoidPtr};
+use crate::mem::{ConstVoidPtr, MutPtr, MutVoidPtr};
 use crate::objc::id;
 use crate::Environment;
 
-/// Usually a FourCC.
 type AudioServicesPropertyID = u32;
 type SystemSoundID = u32;
+type AudioServicesSystemSoundCompletionProc = u32; // guest function pointer
 
 const kAudioServicesUnsupportedPropertyError: OSStatus = fourcc(b"pty?") as _;
+const kAudioServicesBadSystemSoundIDError: OSStatus = fourcc(b"!ids") as _;
 const kSystemSoundID_Vibrate: SystemSoundID = 0x00000FFF;
+const kSystemSoundID_UserPreferredAlert: SystemSoundID = 0x00001000;
 
 fn AudioServicesGetProperty(
     _env: &mut Environment,
     in_property_id: AudioServicesPropertyID,
     _in_specifier_size: u32,
-    _in_specifier: crate::mem::ConstVoidPtr,
+    _in_specifier: ConstVoidPtr,
     _io_property_data_size: MutPtr<u32>,
     _out_property_data: MutVoidPtr,
 ) -> OSStatus {
-    // Некоторые игры (например, Crash Bandicoot) запрашивают этот ID.
     if in_property_id == 0xfff {
         kAudioServicesUnsupportedPropertyError
     } else {
-        log!("AudioServicesGetProperty: property {} is unimplemented", 
-             in_property_id);
-        0 // Возвращаем Success (noErr), чтобы избежать паники
+        log!("AudioServicesGetProperty: property {} is unimplemented", in_property_id);
+        0
     }
+}
+
+fn AudioServicesSetProperty(
+    _env: &mut Environment,
+    in_property_id: AudioServicesPropertyID,
+    _in_specifier_size: u32,
+    _in_specifier: ConstVoidPtr,
+    _in_property_data_size: u32,
+    _in_property_data: ConstVoidPtr,
+) -> OSStatus {
+    log!("AudioServicesSetProperty: property {} is unimplemented", in_property_id);
+    0
 }
 
 fn AudioServicesCreateSystemSoundID(
@@ -44,34 +55,58 @@ fn AudioServicesCreateSystemSoundID(
 ) -> OSStatus {
     log!("AudioToolbox: AudioServicesCreateSystemSoundID stubbed");
     if !out_system_sound_id.is_null() {
-        // Записываем фейковый ID (например, 1001), 
-        // чтобы игра получила валидный дескриптор.
         env.mem.write(out_system_sound_id, 1001);
     }
-    0 // noErr
+    0
 }
 
 fn AudioServicesDisposeSystemSoundID(
     _env: &mut Environment,
     _in_system_sound_id: SystemSoundID,
 ) -> OSStatus {
-    // Просто заглушка для очистки ресурсов.
-    0 // noErr
+    0
 }
 
 fn AudioServicesPlaySystemSound(_env: &mut Environment, in_system_sound_id: SystemSoundID) {
     if in_system_sound_id == kSystemSoundID_Vibrate {
         log!("TODO: vibration (AudioServicesPlaySystemSound)");
+    } else if in_system_sound_id == kSystemSoundID_UserPreferredAlert {
+        log!("TODO: alert sound (AudioServicesPlaySystemSound)");
     } else {
-        log!("AudioToolbox: Playing system sound ID: {} (stub)", 
-             in_system_sound_id);
+        log!("AudioToolbox: Playing system sound ID: {} (stub)", in_system_sound_id);
     }
 }
 
+fn AudioServicesPlayAlertSound(_env: &mut Environment, in_system_sound_id: SystemSoundID) {
+    log!("AudioToolbox: AudioServicesPlayAlertSound: ID {} (stub)", in_system_sound_id);
+}
+
+fn AudioServicesAddSystemSoundCompletion(
+    _env: &mut Environment,
+    _in_system_sound_id: SystemSoundID,
+    _in_run_loop: MutVoidPtr,
+    _in_run_loop_mode: MutVoidPtr,
+    _in_completion_routine: AudioServicesSystemSoundCompletionProc,
+    _in_client_data: MutVoidPtr,
+) -> OSStatus {
+    log!("AudioToolbox: AudioServicesAddSystemSoundCompletion stubbed (completion will never fire)");
+    0
+}
+
+fn AudioServicesRemoveSystemSoundCompletion(
+    _env: &mut Environment,
+    _in_system_sound_id: SystemSoundID,
+) {
+    log!("AudioToolbox: AudioServicesRemoveSystemSoundCompletion stubbed");
+}
+
 pub const FUNCTIONS: FunctionExports = &[
-    // Количество "_" соответствует количеству аргументов ПОСЛЕ "env".
-    export_c_func!(AudioServicesGetProperty(_, _, _, _, _)),      // 5 аргументов
-    export_c_func!(AudioServicesCreateSystemSoundID(_, _)),       // 2 аргумента
-    export_c_func!(AudioServicesDisposeSystemSoundID(_)),         // 1 аргумент
-    export_c_func!(AudioServicesPlaySystemSound(_)),              // 1 аргумент
+    export_c_func!(AudioServicesGetProperty(_, _, _, _, _)),
+    export_c_func!(AudioServicesSetProperty(_, _, _, _, _)),
+    export_c_func!(AudioServicesCreateSystemSoundID(_, _)),
+    export_c_func!(AudioServicesDisposeSystemSoundID(_)),
+    export_c_func!(AudioServicesPlaySystemSound(_)),
+    export_c_func!(AudioServicesPlayAlertSound(_)),
+    export_c_func!(AudioServicesAddSystemSoundCompletion(_, _, _, _, _)),
+    export_c_func!(AudioServicesRemoveSystemSoundCompletion(_)),
 ];
