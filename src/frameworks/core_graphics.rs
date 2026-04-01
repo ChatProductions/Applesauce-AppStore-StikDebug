d2e7f29acf14fe8e931a5b12cdcab2f7c205445b
masterfanaedoodledash-cmd <masterfanaedoodledash@gmail.com>
2026-04-01 22:44:33 +0200

    Update core_graphics.rs

--- src/frameworks/core_graphics.rs
+++ src/frameworks/core_graphics.rs
@@ -15,6 +15,7 @@ pub mod cg_color_space;
 pub mod cg_context;
 pub mod cg_data_provider;
 pub mod cg_geometry;
+pub mod cg_gradient;
 pub mod cg_image;
 pub mod cg_path;
 
@@ -26,6 +27,7 @@ pub const DYLIB: crate::dyld::HostDylib = crate::dyld::HostDylib {
         cg_color::CLASSES,
         cg_color_space::CLASSES,
         cg_context::CLASSES,
+        cg_gradient::CLASSES,
         cg_image::CLASSES,
         cg_path::CLASSES,
     ],
@@ -42,6 +44,7 @@ pub const DYLIB: crate::dyld::HostDylib = crate::dyld::HostDylib {
         cg_context::FUNCTIONS,
         cg_data_provider::FUNCTIONS,
         cg_geometry::FUNCTIONS,
+        cg_gradient::FUNCTIONS,
         cg_image::FUNCTIONS,
         cg_path::FUNCTIONS,
     ],
