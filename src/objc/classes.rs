e4ab5dc41da63e8174d3857f34b02b9466c91b51
whyineedname <emssaoig1@gmail.com>
2026-04-02 15:01:17 +0300

    imp object_getClass

--- src/objc/classes.rs
+++ src/objc/classes.rs
@@ -695,3 +695,14 @@ pub fn objc_getClass(env: &mut crate::Environment, name: ConstPtr<u8>) -> Class 
     nil
 }
 
+pub fn object_getClass(env: &mut crate::Environment, obj: id) -> Class {
+    if obj.is_null() {
+        return nil;
+    }
+    
+    // В Objective-C любой объект в памяти (id) начинается с указателя isa.
+    // Приводим указатель, читаем из памяти гостя структуру objc_object 
+    // и просто возвращаем её поле isa.
+    let objc_obj: objc_object = env.mem.read(obj.cast());
+    objc_obj.isa
+}
