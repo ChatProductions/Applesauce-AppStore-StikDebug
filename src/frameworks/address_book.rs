b45df7d512b27cd1f2badc58d8aed1c0d517274d
j92580498-max <j92580498@gmail.com>
2026-04-02 13:41:58 +0300

    Update address_book.rs

--- src/frameworks/address_book.rs
+++ src/frameworks/address_book.rs
@@ -1,8 +1,10 @@
 /*
  * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * License, v. 2.0.
+ * If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
+#![allow(dead_code)]
 //! AddressBook.framework stub
 
 use crate::dyld::{export_c_func, FunctionExports, HostDylib};
