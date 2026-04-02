69fc39f6a3ea473ac6a57f3a03b35e962ae29cf7
masterfanaedoodledash-cmd <masterfanaedoodledash@gmail.com>
2026-04-02 17:37:57 +0200

    Update ui_tab_bar_item.rs

--- src/frameworks/uikit/ui_tab_bar_item.rs
+++ src/frameworks/uikit/ui_tab_bar_item.rs
@@ -6,6 +6,7 @@
 //! `UITabBarItem`.
 
 use crate::frameworks::foundation::NSInteger;
+use crate::frameworks::foundation::ns_string::get_static_str;
 use crate::objc::{
     id, objc_classes, release, retain, nil, ClassExports, HostObject, NSZonePtr,
 };
