7b9e125c27b3908df725a961cfc4eaef3a3905cb
whyineedname <emssaoig1@gmail.com>
2026-04-02 17:46:34 +0300

    Update ns_operation.rs

--- src/frameworks/foundation/ns_operation.rs
+++ src/frameworks/foundation/ns_operation.rs
@@ -13,23 +13,19 @@ use crate::Environment;
 #[derive(Debug, Default)]
 struct NSInvocationOperationHostObject {
     target: id,
-    selector: SEL,
+    selector: Option<SEL>, // Используем Option, так как у SEL нет Default
     arg: id,
 }
 impl HostObject for NSInvocationOperationHostObject {}
 
 #[derive(Debug, Default)]
-struct NSOperationQueueHostObject {
-    // В полноценном многопоточном режиме здесь бы хранилась очередь (Vec<id>).
-    // Но для HLE синхронного выполнения состояние можно оставить пустым.
-}
+struct NSOperationQueueHostObject {}
 impl HostObject for NSOperationQueueHostObject {}
 
 pub const CLASSES: ClassExports = objc_classes! {
 
 (env, this, _cmd);
 
-// Базовый класс операции
 @implementation NSOperation: NSObject
 
 - (id)init {
@@ -37,21 +33,19 @@ pub const CLASSES: ClassExports = objc_classes! {
 }
 
 - (())start {
-    // По дефолту start вызывает main
+    // В HLE реализации мы просто вызываем main() сразу
     () = msg![env; this main];
 }
 
 - (())main {
-    // Должен переопределяться сабклассами
+    // Базовая реализация ничего не делает
 }
 
 - (())cancel {
-    // Заглушка, так как мы выполняем операции сразу при добавлении
 }
 
 @end
 
-// Наш недостающий класс
 @implementation NSInvocationOperation: NSOperation
 
 + (id)allocWithZone:(NSZonePtr)_zone {
@@ -64,40 +58,48 @@ pub const CLASSES: ClassExports = objc_classes! {
     if this != nil {
         retain(env, target);
         retain(env, arg);
+        
         let host_object = env.objc.borrow_mut::<NSInvocationOperationHostObject>(this);
         host_object.target = target;
-        host_object.selector = sel;
+        host_object.selector = Some(sel);
         host_object.arg = arg;
     }
     this
 }
 
 - (())dealloc {
-    let host_object = env.objc.borrow::<NSInvocationOperationHostObject>(this);
-    release(env, host_object.target);
-    release(env, host_object.arg);
+    // Используем временный блок { }, чтобы заимствование host_object завершилось
+    // до того, как мы вызовем release(env, ...), который требует монопольный доступ к env.
+    let (target, arg) = {
+        let host_object = env.objc.borrow::<NSInvocationOperationHostObject>(this);
+        (host_object.target, host_object.arg)
+    };
+    
+    release(env, target);
+    release(env, arg);
     env.objc.dealloc_object(this, &mut env.mem)
 }
 
-// Фактическое выполнение селектора с переданными аргументами
 - (())main {
-    let host_object = env.objc.borrow::<NSInvocationOperationHostObject>(this);
-    let target = host_object.target;
-    let sel = host_object.selector;
-    let arg = host_object.arg;
+    // Извлекаем данные во временные переменные, чтобы освободить env для макроса msg!
+    let (target, sel_opt, arg) = {
+        let host_object = env.objc.borrow::<NSInvocationOperationHostObject>(this);
+        (host_object.target, host_object.selector, host_object.arg)
+    };
 
     if target != nil {
-        if arg != nil {
-            let _: id = msg![env; target performSelector:sel withObject:arg];
-        } else {
-            let _: id = msg![env; target performSelector:sel];
+        if let Some(sel) = sel_opt {
+            if arg != nil {
+                let _: id = msg![env; target performSelector:sel withObject:arg];
+            } else {
+                let _: id = msg![env; target performSelector:sel];
+            }
         }
     }
 }
 
 @end
 
-// Класс очереди, через который игра будет пытаться эти операции запускать
 @implementation NSOperationQueue: NSObject
 
 + (id)allocWithZone:(NSZonePtr)_zone {
@@ -109,22 +111,17 @@ pub const CLASSES: ClassExports = objc_classes! {
     this
 }
 
-- (())addOperation:(id)op { // NSOperation*
+- (())addOperation:(id)op { 
     retain(env, op);
-    
-    // В оригинале операции уходят в асинхронный пул потоков.
-    // В HLE (без сложного шедулера) мы просто "пробиваем" операцию синхронно.
-    // Это безопасно для подавляющего большинства старых игр.
+    // Для простоты выполняем операцию синхронно
     () = msg![env; op start];
-    
     release(env, op);
 }
 
 - (())setMaxConcurrentOperationCount:(i32)_count {
-    // Метод настройки пула. При нашей синхронной реализации он ни на что не влияет.
+    // Игнорируем в данной реализации
 }
 
 @end
 
 };
-  
