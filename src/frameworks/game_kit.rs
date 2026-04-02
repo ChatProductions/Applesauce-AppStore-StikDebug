1c7770e7831c22493f156c611e2b7e81ef7b9ad3
masterfanaedoodledash-cmd <masterfanaedoodledash@gmail.com>
2026-04-02 21:45:48 +0200

    Update game_kit.rs

--- src/frameworks/game_kit.rs
+++ src/frameworks/game_kit.rs
@@ -18,7 +18,7 @@ mod gk_score;
 pub const DYLIB: crate::dyld::HostDylib = crate::dyld::HostDylib {
     path: "/System/Library/Frameworks/GameKit.framework/GameKit",
     aliases: &[],
-    class_exports: &[ad_banner_view:CLASSES, gk_leaderboard_view_controller::CLASSES, gk_local_player::CLASSES, gk_score::CLASSES],
+    class_exports: &[ad_banner_view::CLASSES, gk_leaderboard_view_controller::CLASSES, gk_local_player::CLASSES, gk_score::CLASSES],
     constant_exports: &[gk_local_player::CONSTANTS],
     function_exports: &[],
 };
