5653426eed2e229def38a25d385b3da2124fa712
masterfanaedoodledash-cmd <masterfanaedoodledash@gmail.com>
2026-04-02 07:14:03 +0200

    Update store_kit.rs

--- src/frameworks/store_kit.rs
+++ src/frameworks/store_kit.rs
@@ -6,11 +6,12 @@
 //! StoreKit
 
 mod sk_payment_queue;
+mod sk_product;
 
 pub const DYLIB: crate::dyld::HostDylib = crate::dyld::HostDylib {
     path: "/System/Library/Frameworks/StoreKit.framework/StoreKit",
     aliases: &[],
-    class_exports: &[sk_payment_queue::CLASSES],
+    class_exports: &[sk_payment_queue::CLASSES, sk_product::CLASSES],
     constant_exports: &[],
     function_exports: &[],
 };
