5d5d56fdb9570fb2adb5211a943751dee0cd5821
j92580498-max <j92580498@gmail.com>
2026-04-03 21:33:45 +0300

    Update ns_value.rs

--- src/frameworks/foundation/ns_value.rs
+++ src/frameworks/foundation/ns_value.rs
@@ -497,8 +497,12 @@ pub const CLASSES: ClassExports = objc_classes! {
         NSNumberHostObject::Short(_) => b"s\0",
         NSNumberHostObject::UnsignedShort(_) => b"S\0",
     };
-    // Allocate the C-string inside guest memory and return its pointer.
-    env.mem.alloc_and_write(*typ).cast_void().cast_const()
+    
+    // Переводим [u8; 2] в u16 (little-endian), так как u16 поддерживает SafeWrite
+    let typ_val = u16::from_le_bytes(*typ);
+    
+    // Выделяем память под u16 и возвращаем указатель
+    env.mem.alloc_and_write(typ_val).cast_void().cast_const()
 }
 
 // TODO: accessors etc
