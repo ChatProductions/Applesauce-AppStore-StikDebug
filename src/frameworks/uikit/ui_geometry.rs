ce0ad74568f4285f62b4b78d4f0c02b7abc79f2b
j92580498-max <j92580498@gmail.com>
2026-04-03 21:42:04 +0300

    Update ui_geometry.rs

--- src/frameworks/uikit/ui_geometry.rs
+++ src/frameworks/uikit/ui_geometry.rs
@@ -12,7 +12,6 @@ use crate::Environment;
 #[derive(Default)]
 pub struct State {
     pub default_malloc_zone: Option<MutVoidPtr>,
-    // ...
 }
 
 // Apple's documentation says all of these return zeroes if the input is not
@@ -50,21 +49,21 @@ pub fn NSStringFromCGRect(env: &mut Environment, rect: CGRect) -> id {
 }
 
 fn NSDefaultMallocZone(env: &mut Environment) -> MutVoidPtr {
-     if let Some(ptr) = env.framework_state.uikit.ui_geometry.default_malloc_zone {
+    if let Some(ptr) = env.framework_state.uikit.ui_geometry.default_malloc_zone {
         return ptr;
     }
     let ptr = env.mem.alloc(4);
-    env.framework_state.env.framework_state.uikit_state.uikit.default_malloc_zone = Some(ptr);
+    env.framework_state.uikit.ui_geometry.default_malloc_zone = Some(ptr);
     log_dbg!("NSDefaultMallocZone: allocated dummy zone at {:?}", ptr);
     ptr
 }
 
 pub const FUNCTIONS: FunctionExports = &[
-    export_c_func!(CGPointFromString(_)),
-    export_c_func!(CGSizeFromString(_)),
-    export_c_func!(CGRectFromString(_)),
-    export_c_func!(NSStringFromCGPoint(_)),
-    export_c_func!(NSStringFromCGSize(_)),
-    export_c_func!(NSStringFromCGRect(_)),
-    export_c_func!(NSDefaultMallocZone()),
+    export_c_func!(CGPointFromString(_, _)),
+    export_c_func!(CGSizeFromString(_, _)),
+    export_c_func!(CGRectFromString(_, _)),
+    export_c_func!(NSStringFromCGPoint(_, _)),
+    export_c_func!(NSStringFromCGSize(_, _)),
+    export_c_func!(NSStringFromCGRect(_, _)),
+    export_c_func!(NSDefaultMallocZone(_)),
 ];
