920fae47176dd3c7f346a36a45684561eaf5d5d0
masterfanaedoodledash-cmd <masterfanaedoodledash@gmail.com>
2026-04-04 13:50:00 +0200

    Update objc.rs

--- src/objc.rs
+++ src/objc.rs
@@ -30,7 +30,7 @@ mod properties;
 mod selectors;
 mod synchronization;
 
-pub use classes::{objc_classes, objc_getClass, object_getClass, objc_setProperty_nonatomic, objc_retainAutoreleasedReturnValue, objc_autoreleasePoolPush, objc_exception_throw, objc_begin_catch, class_getSuperclass, class_getInstanceMethod, Class, ClassExports, ClassTemplate};
+pub use classes::{objc_classes, objc_getClass, object_getClass, objc_setProperty_nonatomic, objc_retainAutoreleasedReturnValue, objc_autoreleasePoolPush, objc_exception_throw, objc_begin_catch, objc_end_catch, class_getSuperclass, class_getInstanceMethod, Class, ClassExports, ClassTemplate};
 pub use messages::{
     autorelease, msg, msg_class, msg_send, msg_send_no_type_checking, msg_send_super2, msg_super,
     objc_super, release, retain,
@@ -145,6 +145,7 @@ const FUNCTIONS: FunctionExports = &[
     export_c_func!(objc_setProperty_nonatomic(_)),
     export_c_func!(objc_exception_throw(_)),
     export_c_func!(objc_begin_catch(_)),
+    export_c_func!(objc_end_catch(_)),
     export_c_func!(class_getSuperclass(_)),
     export_c_func!(class_getInstanceMethod(_, _)),
     export_c_func!(_Block_object_dispose(_, _)),
