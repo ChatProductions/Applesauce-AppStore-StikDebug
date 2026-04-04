a3fa8d5548bddfc45b4bb2994435cedd7415ff6f
masterfanaedoodledash-cmd <masterfanaedoodledash@gmail.com>
2026-04-04 21:21:05 +0200

    Update ui_page_control.rs

--- src/frameworks/uikit/ui_view/ui_page_control.rs
+++ src/frameworks/uikit/ui_view/ui_page_control.rs
@@ -6,7 +6,21 @@
 //! `UIPageControl`.
 
 use crate::frameworks::foundation::NSInteger;
-use crate::objc::{objc_classes, ClassExports};
+use crate::objc::{
+    id, msg_class, nil, objc_classes, release, retain, ClassExports, HostObject, NSZonePtr,
+};
+
+struct UIPageControlHostObject {
+    number_of_pages: NSInteger,
+    current_page: NSInteger,
+    hides_for_single_page: bool,
+    defers_current_page_display: bool,
+    /// UIColor* for the dots that are not current.
+    page_indicator_tint_color: id,
+    /// UIColor* for the current-page dot.
+    current_page_indicator_tint_color: id,
+}
+impl HostObject for UIPageControlHostObject {}
 
 pub const CLASSES: ClassExports = objc_classes! {
 
@@ -14,12 +28,144 @@ pub const CLASSES: ClassExports = objc_classes! {
 
 @implementation UIPageControl: UIControl
 
-- (())setCurrentPage:(NSInteger)currentPage {
-    log!("TODO: [(UIPageControl*) {:?} setCurrentPage:{}]", this, currentPage);
++ (id)allocWithZone:(NSZonePtr)_zone {
+    let host_object = Box::new(UIPageControlHostObject {
+        number_of_pages: 0,
+        current_page: 0,
+        hides_for_single_page: false,
+        defers_current_page_display: false,
+        page_indicator_tint_color: nil,
+        current_page_indicator_tint_color: nil,
+    });
+    env.objc.alloc_object(this, host_object, &mut env.mem)
 }
 
-- (())setNumberOfPages:(NSInteger)numberOfPages {
-    log!("TODO: [(UIPageControl*) {:?} setNumberOfPages:{}]", this, numberOfPages);
+- (id)init {
+    this
+}
+
+- (())dealloc {
+    let host = env.objc.borrow::<UIPageControlHostObject>(this);
+    let (tint, current_tint) = (
+        host.page_indicator_tint_color,
+        host.current_page_indicator_tint_color,
+    );
+    release(env, tint);
+    release(env, current_tint);
+    env.objc.dealloc_object(this, &mut env.mem)
+}
+
+// MARK: - Page count
+
+- (NSInteger)numberOfPages {
+    env.objc.borrow::<UIPageControlHostObject>(this).number_of_pages
+}
+
+- (())setNumberOfPages:(NSInteger)number_of_pages {
+    log_dbg!("UIPageControl setNumberOfPages:{}", number_of_pages);
+    let host = env.objc.borrow_mut::<UIPageControlHostObject>(this);
+    host.number_of_pages = number_of_pages;
+    // Clamp current page to valid range.
+    if number_of_pages == 0 {
+        host.current_page = 0;
+    } else if host.current_page >= number_of_pages {
+        host.current_page = number_of_pages - 1;
+    }
+}
+
+// MARK: - Current page
+
+- (NSInteger)currentPage {
+    env.objc.borrow::<UIPageControlHostObject>(this).current_page
+}
+
+- (())setCurrentPage:(NSInteger)current_page {
+    log_dbg!("UIPageControl setCurrentPage:{}", current_page);
+    let n = env.objc.borrow::<UIPageControlHostObject>(this).number_of_pages;
+    let clamped = if n == 0 {
+        0
+    } else {
+        current_page.max(0).min(n - 1)
+    };
+    env.objc.borrow_mut::<UIPageControlHostObject>(this).current_page = clamped;
+}
+
+// MARK: - Display options
+
+- (bool)hidesForSinglePage {
+    env.objc.borrow::<UIPageControlHostObject>(this).hides_for_single_page
+}
+
+- (())setHidesForSinglePage:(bool)hides {
+    env.objc.borrow_mut::<UIPageControlHostObject>(this).hides_for_single_page = hides;
+}
+
+- (bool)defersCurrentPageDisplay {
+    env.objc.borrow::<UIPageControlHostObject>(this).defers_current_page_display
+}
+
+- (())setDefersCurrentPageDisplay:(bool)defers {
+    env.objc.borrow_mut::<UIPageControlHostObject>(this).defers_current_page_display = defers;
+}
+
+// When defersCurrentPageDisplay is YES the app calls this to commit the
+// pending page change to the display. We store it immediately either way.
+- (())updateCurrentPageDisplay {
+    log_dbg!("UIPageControl updateCurrentPageDisplay");
+}
+
+// MARK: - Tint colors
+
+- (id)pageIndicatorTintColor { // UIColor*
+    env.objc.borrow::<UIPageControlHostObject>(this).page_indicator_tint_color
+}
+
+- (())setPageIndicatorTintColor:(id)color { // UIColor*
+    let old = env.objc.borrow::<UIPageControlHostObject>(this).page_indicator_tint_color;
+    release(env, old);
+    retain(env, color);
+    env.objc.borrow_mut::<UIPageControlHostObject>(this).page_indicator_tint_color = color;
+}
+
+- (id)currentPageIndicatorTintColor { // UIColor*
+    env.objc.borrow::<UIPageControlHostObject>(this).current_page_indicator_tint_color
+}
+
+- (())setCurrentPageIndicatorTintColor:(id)color { // UIColor*
+    let old = env.objc.borrow::<UIPageControlHostObject>(this)
+        .current_page_indicator_tint_color;
+    release(env, old);
+    retain(env, color);
+    env.objc.borrow_mut::<UIPageControlHostObject>(this)
+        .current_page_indicator_tint_color = color;
+}
+
+// MARK: - Size hint
+
+// Returns the minimum size needed to display the dots for a given page count.
+// Matches UIKit's fixed 7pt dot size with 9pt spacing.
++ (id)sizeForNumberOfPages:(NSInteger)page_count { // returns CGSize
+    let width: f32 = (page_count.max(0) as f32) * 9.0 - 2.0; // 7pt dot + 2pt gap
+    let size = crate::frameworks::core_graphics::CGSize {
+        width: width.max(0.0),
+        height: 36.0,
+    };
+    msg_class![env; NSValue valueWithCGSize:size]
+}
+
+// MARK: - Description
+
+- (id)description {
+    let (n, cur) = {
+        let h = env.objc.borrow::<UIPageControlHostObject>(this);
+        (h.number_of_pages, h.current_page)
+    };
+    let s = format!(
+        "<UIPageControl: {:?}; numberOfPages={}; currentPage={}>",
+        this, n, cur
+    );
+    let cstr = env.mem.alloc_and_write_cstr(s.as_bytes());
+    msg_class![env; NSString stringWithUTF8String:cstr]
 }
 
 @end
