10504d2030d869f33ccca99a8bdf67bf7c68167a
masterfanaedoodledash-cmd <masterfanaedoodledash@gmail.com>
2026-04-06 14:56:47 +0200

    Update audio_services.rs

--- src/frameworks/audio_toolbox/audio_services.rs
+++ src/frameworks/audio_toolbox/audio_services.rs
@@ -4,7 +4,6 @@
  * If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
-#![allow(dead_code)]
 //! `AudioServices.h` (Audio Services)
 
 use crate::dyld::{export_c_func, FunctionExports};
