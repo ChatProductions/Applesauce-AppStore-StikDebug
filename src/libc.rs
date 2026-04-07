7663f46e88746b308479234a18a614c955146f55
masterfanaedoodledash-cmd <masterfanaedoodledash@gmail.com>
2026-04-07 13:15:11 +0200

    Update libc.rs

--- src/libc.rs
+++ src/libc.rs
@@ -115,7 +115,6 @@ pub struct State {
     dirent: dirent::State,
     keymgr: keymgr::State,
     math: math::State,
-    netdb: netdb::State,
     posix_io: posix_io::State,
     pub pthread: pthread::State,
     pub semaphore: semaphore::State,
