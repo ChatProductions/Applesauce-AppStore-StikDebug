990eaefe2e8ab2dd466c8cacb9ed5db7a87f5beb
masterfanaedoodledash-cmd <masterfanaedoodledash@gmail.com>
2026-04-07 21:03:31 +0200

    Update ns_bundle.rs

--- src/frameworks/foundation/ns_bundle.rs
+++ src/frameworks/foundation/ns_bundle.rs
@@ -117,6 +117,98 @@ pub const CLASSES: ClassExports = objc_classes! {
     nil
 }
 
++ (id)bundleURL {
+    if let Some(url) = env.objc.borrow::<NSBundleHostObject>(this).bundle_url {
+        url
+    } else {
+        let bundle_path: id = msg![env; this bundlePath];
+        let new: id = msg_class![env; NSURL alloc];
+        let new: id = msg![env; new initFileURLWithPath:bundle_path];
+        env.objc.borrow_mut::<NSBundleHostObject>(this).bundle_url = Some(new);
+        new
+    }
+}
+
++ (id)bundlePath {
+    env.objc.borrow::<NSBundleHostObject>(this).bundle_path
+}
+
++ (id)resourcePath {
+    msg![env; this bundlePath]
+}
+
++ (id)resourceURL {
+    msg![env; this bundleURL]
+}
+
++ (id)executablePath {
+    let exec_path_str = env.bundle.executable_path().as_str().to_string();
+    let exec_path = from_rust_string(env, exec_path_str);
+    autorelease(env, exec_path)
+}
+
++ (id)executableURL {
+    let exec_path: id = msg![env; this executablePath];
+    if exec_path == nil {
+        return nil;
+    }
+    let url: id = msg_class![env; NSURL alloc];
+    let url: id = msg![env; url initFileURLWithPath:exec_path];
+    autorelease(env, url)
+}
+
++ (id)pathForResource:(id)name          // NSString*
+               ofType:(id)extension     // NSString*
+          inDirectory:(id)directory {   // NSString*
+    // assert!(name != nil);
+
+    let path = path_for_resource_helper(env, this, name, nil, directory, extension);
+    if path != nil {
+        return path;
+    }
+
+    // Try preferred languages in order.
+    let langs: id = msg_class![env; NSLocale preferredLanguages];
+    let lang_count: NSUInteger = msg![env; langs count];
+    let mut unknown_codes = HashSet::new();
+    for i in 0..lang_count {
+        let lang_code: id = msg![env; langs objectAtIndex:i];
+        let lang_code_str = ns_string::to_rust_string(env, lang_code);
+        if let Some(&(_, lprojs)) = LANG_ID_TO_LANG_PROJ
+            .iter()
+            .find(|&&(code, _)| code == lang_code_str)
+        {
+            for lproj in lprojs {
+                let lproj_ns: id = ns_string::get_static_str(env, lproj);
+                let localized_path =
+                    path_for_resource_helper(env, this, name, lproj_ns, directory, extension);
+                if localized_path != nil {
+                    return localized_path;
+                }
+            }
+        } else {
+            unknown_codes.insert(lang_code_str.into_owned());
+        }
+    }
+
+    if !unknown_codes.is_empty() {
+        log!(
+            "TODO: language codes {:?} aren't mapped to a language name, falling back to English",
+            unknown_codes
+        );
+    }
+
+    // Fallback to English.
+    for lproj in ["English.lproj", "en.lproj"] {
+        let lproj_ns: id = ns_string::get_static_str(env, lproj);
+        let path = path_for_resource_helper(env, this, name, lproj_ns, directory, extension);
+        if path != nil {
+            return path;
+        }
+    }
+    nil
+}
+
 + (id)allBundles {
     // Return an array containing only the main bundle.
     let main: id = msg_class![env; NSBundle mainBundle];
