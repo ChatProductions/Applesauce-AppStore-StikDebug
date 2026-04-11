a1654fb32a5b5036fc21d039b3a770d4c8ce60c2
j92580498-max <j92580498@gmail.com>
2026-04-11 18:21:38 +0300

    Update log.rs

--- src/log.rs
+++ src/log.rs
@@ -113,4 +113,11 @@ macro_rules! echo_no_panic {
 
 /// Put modules to enable [log_dbg] for here, e.g. "touchHLE::mem" to see when
 /// memory is allocated and freed.
-pub const ENABLED_MODULES: &[&str] = &[];
+pub const ENABLED_MODULES: &[&str] = &[
+    "touchHLE::abi",
+    "touchHLE::dyld",
+    "touchHLE::objc",
+    "touchHLE::objc::msg_send",
+    "touchHLE::objc::msg_send_arm32",
+    "touchHLE::objc::classes",
+];
