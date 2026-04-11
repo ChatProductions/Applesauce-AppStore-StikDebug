1e202c700d5fd9d29446ac956ca83903fe1164cb
j92580498-max <j92580498@gmail.com>
2026-04-11 14:26:01 +0300

    Update cxxabi.rs

--- src/libc/cxxabi.rs
+++ src/libc/cxxabi.rs
@@ -1,13 +1,6 @@
 /*
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ * `cxxabi.h`
  */
-//! `cxxabi.h`
-//!
-//! Resources:
-//! - [Itanium C++ ABI specification](https://itanium-cxx-abi.github.io/cxx-abi/abi.html#dso-dtor-runtime-api)
-
 use crate::abi::GuestFunction;
 use crate::dyld::{export_c_func, FunctionExports};
 use crate::mem::MutVoidPtr;
@@ -15,47 +8,35 @@ use crate::Environment;
 
 fn __cxa_atexit(
     _env: &mut Environment,
-    func: GuestFunction, // void (*func)(void *)
+    func: GuestFunction,
     p: MutVoidPtr,
     d: MutVoidPtr,
 ) -> i32 {
-    // TODO: when this is implemented, make sure it's properly compatible with
-    // C atexit.
-    log!(
-        "TODO: __cxa_atexit({:?}, {:?}, {:?}) (unimplemented)",
-        func,
-        p,
-        d
-    );
-    0 // success
+    // Просто возвращаем успех (0), чтобы игра не падала.
+    // Лог убран, чтобы не засорять консоль.
+    0 
 }
 
 fn __cxa_finalize(_env: &mut Environment, d: MutVoidPtr) {
-    log!("TODO: __cxa_finalize({:?}) (unimplemented)", d);
+    // Пустая заглушка
 }
 
-// --- ДОБАВЛЕННЫЕ ЗАГЛУШКИ ДЛЯ SjLj ИСКЛЮЧЕНИЙ ---
-// В iOS C-функции получают дополнительное подчеркивание при сборке.
-// Поэтому для экспорта символа "__Unwind_SjLj_Register" в макросе
-// имя функции должно начинаться только с ОДНОГО подчеркивания.
+// --- Заглушки для SjLj исключений ---
 
 #[allow(non_snake_case)]
-fn _Unwind_SjLj_Register(_env: &mut Environment, _context: MutVoidPtr) {
-}
+fn _Unwind_SjLj_Register(_env: &mut Environment, _context: MutVoidPtr) {}
 
 #[allow(non_snake_case)]
-fn _Unwind_SjLj_Unregister(_env: &mut Environment, _context: MutVoidPtr) {
-}
+fn _Unwind_SjLj_Unregister(_env: &mut Environment, _context: MutVoidPtr) {}
 
 #[allow(non_snake_case)]
-fn _Unwind_SjLj_Resume(_env: &mut Environment, _context: MutVoidPtr) {
-}
+fn _Unwind_SjLj_Resume(_env: &mut Environment, _context: MutVoidPtr) {}
+
+// --- Экспорт функций ---
 
 pub const FUNCTIONS: FunctionExports = &[
     export_c_func!(__cxa_atexit(_, _, _)),
     export_c_func!(__cxa_finalize(_)),
-    
-    // Экспортируем наши заглушки с правильными именами (1 подчеркивание):
     export_c_func!(_Unwind_SjLj_Register(_)),
     export_c_func!(_Unwind_SjLj_Unregister(_)),
     export_c_func!(_Unwind_SjLj_Resume(_)),
