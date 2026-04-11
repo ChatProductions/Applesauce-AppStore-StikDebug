45f90fb9713e7e09011e55d45faf3d30e4837370
j92580498-max <j92580498@gmail.com>
2026-04-11 19:21:46 +0300

    Update log.rs

--- src/log.rs
+++ src/log.rs
@@ -113,8 +113,4 @@ macro_rules! echo_no_panic {
 
 /// Put modules to enable [log_dbg] for here, e.g. "touchHLE::mem" to see when
 /// memory is allocated and freed.
-pub const ENABLED_MODULES: &[&str] = &[
-    "touchHLE::libc",
-    "touchHLE::libc::stdio",
-    "touchHLE::fs",
-];
+pub const ENABLED_MODULES: &[&str] = &[];
