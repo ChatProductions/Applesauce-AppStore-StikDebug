4df049b04ec9ab997afa4ce6ce1ffe8c2dde2f20
whyineedname <emssaoig1@gmail.com>
2026-04-11 14:51:45 +0300

    Update game_kit.rs

--- src/frameworks/game_kit.rs
+++ src/frameworks/game_kit.rs
@@ -14,11 +14,12 @@ pub mod ad_banner_view;
 pub mod gk_leaderboard_view_controller;
 mod gk_local_player;
 mod gk_score;
+mod gk_session;
 
 pub const DYLIB: crate::dyld::HostDylib = crate::dyld::HostDylib {
     path: "/System/Library/Frameworks/GameKit.framework/GameKit",
     aliases: &[],
-    class_exports: &[ad_banner_view::CLASSES, gk_leaderboard_view_controller::CLASSES, gk_local_player::CLASSES, gk_score::CLASSES],
+    class_exports: &[ad_banner_view::CLASSES, gk_leaderboard_view_controller::CLASSES, gk_local_player::CLASSES, gk_score::CLASSES, gk_session::CLASSES],
     constant_exports: &[gk_local_player::CONSTANTS],
     function_exports: &[],
 };
