57da6495daed52a799d501dd505791040a5bc653
j92580498-max <j92580498@gmail.com>
2026-04-13 00:59:20 +0300

    Update ns_date_formatter.rs

--- src/frameworks/foundation/ns_date_formatter.rs
+++ src/frameworks/foundation/ns_date_formatter.rs
@@ -1,6 +1,7 @@
 /*
  * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * License, v. 2.0.
+ * If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 //! `NSDateFormatter`.
@@ -40,34 +41,81 @@ pub const CLASSES: ClassExports = objc_classes! {
     let &NSDateFormatterHostObject {
         date_format
     } = env.objc.borrow(this);
-    let mut format = ns_string::to_rust_string(env, date_format.unwrap()).to_string().clone();
-    log_dbg!("date_format before: {:?}", format);
+    
+    let format_str = ns_string::to_rust_string(env, date_format.unwrap()).to_string();
+    log_dbg!("date_format before: {:?}", format_str);
 
     let ti: NSTimeInterval = msg![env; date timeIntervalSinceReferenceDate];
+    // Получаем время в UTC (т.к. передаем nil вместо таймзоны)
     let greg_date = CFAbsoluteTimeGetGregorianDate(env, ti, nil);
-    let year = greg_date.year;
-    let month = greg_date.month;
-    let day = greg_date.day;
-    let hour = greg_date.hours;
-    let minute = greg_date.minutes;
-    let second = greg_date.seconds;
+    
+    let mut result = String::new();
+    let mut chars = format_str.chars().peekable();
+    let mut in_quotes = false;
 
-    format = format.replace("yyyy", format!("{year:04}").as_str());
-    format = format.replace("YYYY", format!("{year:04}").as_str());
-    format = format.replace("MM", format!("{month:02}").as_str());
-    format = format.replace("dd", format!("{day:02}").as_str());
-    format = format.replace("HH", format!("{hour:02}").as_str());
-    format = format.replace("mm", format!("{minute:02}").as_str());
-    format = format.replace("ss", format!("{second:02}").as_str());
+    while let Some(c) = chars.next() {
+        // Обработка экранирования через одинарные кавычки
+        if c == '\'' {
+            if chars.peek() == Some(&'\'') {
+                // Два апострофа подряд означают один реальный апостроф в строке
+                result.push('\'');
+                chars.next();
+            } else {
+                // Переключаем режим "внутри кавычек"
+                in_quotes = !in_quotes;
+            }
+            continue;
+        }
 
-    for c in format.chars() {
-        if let pattern @ ('A'..='Z' | 'a'..='z') = c {
-            unimplemented!("date string contains unsubstituted format pattern: {pattern}");
+        // Если мы внутри кавычек (например 'T'), просто добавляем символ как есть
+        if in_quotes {
+            result.push(c);
+            continue;
+        }
+
+        // Если символ - буква, собираем токен (например: yyyy, MM, ZZZZ)
+        if c.is_ascii_alphabetic() {
+            let mut token = String::from(c);
+            while let Some(&next_c) = chars.peek() {
+                if next_c == c {
+                    token.push(chars.next().unwrap());
+                } else {
+                    break;
+                }
+            }
+
+            // Подставляем значения согласно стандарту Unicode TR35
+            match token.as_str() {
+                "yyyy" | "YYYY" => result.push_str(&format!("{:04}", greg_date.year)),
+                "MM" => result.push_str(&format!("{:02}", greg_date.month)),
+                "M" => result.push_str(&format!("{}", greg_date.month)),
+                "dd" => result.push_str(&format!("{:02}", greg_date.day)),
+                "d" => result.push_str(&format!("{}", greg_date.day)),
+                "HH" => result.push_str(&format!("{:02}", greg_date.hours)),
+                "H" => result.push_str(&format!("{}", greg_date.hours)),
+                "mm" => result.push_str(&format!("{:02}", greg_date.minutes)),
+                "m" => result.push_str(&format!("{}", greg_date.minutes)),
+                "ss" => result.push_str(&format!("{:02}", greg_date.seconds)),
+                "s" => result.push_str(&format!("{}", greg_date.seconds)),
+                
+                // Правильная реализация таймзон (UTC/GMT)
+                "Z" | "ZZ" | "ZZZ" => result.push_str("+0000"),
+                "ZZZZ" => result.push_str("GMT+00:00"),
+                "ZZZZZ" => result.push_str("+00:00"),
+                "z" | "zz" | "zzz" | "zzzz" => result.push_str("GMT"),
+                
+                // Если игре нужен паттерн, которого тут нет, она упадет здесь
+                _ => unimplemented!("date string contains unsubstituted format pattern: {}", token),
+            }
+        } else {
+            // Разделители (пробелы, тире, двоеточия) оставляем как есть
+            result.push(c);
         }
     }
-    log_dbg!("date_format after: {:?}", format);
 
-    let res = ns_string::from_rust_string(env, format);
+    log_dbg!("date_format after: {:?}", result);
+
+    let res = ns_string::from_rust_string(env, result);
     autorelease(env, res)
 }
 
