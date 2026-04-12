624f1055f08ef0e1be349092afc78eb8a5d0150e
j92580498-max <j92580498@gmail.com>
2026-04-12 12:39:22 +0300

    Update game_kit.rs

--- src/frameworks/game_kit.rs
+++ src/frameworks/game_kit.rs
@@ -12,14 +12,26 @@
 
 pub mod ad_banner_view;
 pub mod gk_leaderboard_view_controller;
-mod gk_local_player;
+pub mod gk_local_player;
 mod gk_score;
 mod gk_session;
+
+/// Per-process state for the GameKit framework.
+#[derive(Default)]
+pub struct State {
+    pub local_player: gk_local_player::State,
+}
 
 pub const DYLIB: crate::dyld::HostDylib = crate::dyld::HostDylib {
     path: "/System/Library/Frameworks/GameKit.framework/GameKit",
     aliases: &[],
-    class_exports: &[ad_banner_view::CLASSES, gk_leaderboard_view_controller::CLASSES, gk_local_player::CLASSES, gk_score::CLASSES, gk_session::CLASSES],
+    class_exports: &[
+        ad_banner_view::CLASSES,
+        gk_leaderboard_view_controller::CLASSES,
+        gk_local_player::CLASSES,
+        gk_score::CLASSES,
+        gk_session::CLASSES,
+    ],
     constant_exports: &[gk_local_player::CONSTANTS],
     function_exports: &[],
 };
