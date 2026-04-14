c78b31eb353be3a258a46ab350651b4ea01a031e
j92580498-max <j92580498@gmail.com>
2026-04-14 12:05:43 +0300

    Update frameworks.rs

--- src/frameworks.rs
+++ src/frameworks.rs
@@ -6,16 +6,17 @@
 //! Our implementations of various frameworks.
 //!
 //! Each child module should be named after the framework it implements.
-//! It can potentially have multiple child modules itself if it's a particularly
-//! complex framework.
+//! It can potentially have multiple child modules itself if it's a
+//! particularly complex framework.
 //!
 //! See also `dyld/function_lists.rs` and `objc/classes/class_lists.rs`.
 //!
-//! Most modules in here are not going to link to documentation that should be
-//! trivial to find by searching for the class or function name. For example,
-//! the documentation of `NSArray` won't link to the main developer.apple.com
-//! page documenting that class, but if there's something interesting in the
-//! Documentation Archive relating to arrays, that might be linked.
+//! Most modules in here are not going to link to documentation that should
+//! be trivial to find by searching for the class or function name. For
+//! example, the documentation of `NSArray` won't link to the main
+//! developer.apple.com page documenting that class, but if there's
+//! something interesting in the Documentation Archive relating to arrays,
+//! that might be linked.
 
 #![allow(non_upper_case_globals)] // Lots of Apple constants begin with "k"
 #![allow(clippy::enum_variant_names)] // Lots of Apple enums have the same prefix
@@ -25,8 +26,10 @@ pub mod address_book;
 pub mod audio_toolbox;
 pub mod avfoundation;
 pub mod carbon_core;
+pub mod cf_network;
 pub mod common_crypto;
 pub mod core_animation;
+pub mod core_audio;
 pub mod core_audio_types;
 pub mod core_foundation;
 pub mod core_graphics;
@@ -41,11 +44,10 @@ pub mod libxml2;
 pub mod media_player;
 pub mod openal;
 pub mod opengles;
+pub mod security;
 pub mod store_kit;
 pub mod system_configuration;
 pub mod uikit;
-pub mod core_audio;
-pub mod cf_network;
 
 /// Container for state of various child modules
 #[derive(Default)]
