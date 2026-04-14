41f6a5a4aeab1425beb036197cd4404cc2988e75
j92580498-max <j92580498@gmail.com>
2026-04-14 21:28:09 +0300

    Update dyld.rs

--- src/dyld.rs
+++ src/dyld.rs
@@ -51,8 +51,7 @@ pub type HostFunction = &'static dyn CallFromGuest;
 pub type FunctionExports = &'static [(&'static str, HostFunction)];
 
 #[macro_export]
-macro_rules!
-export_c_func {
+macro_rules! export_c_func {
     ($name:ident ($($_:ty),*)) => {
         (
             concat!("_", stringify!($name)),
@@ -63,8 +62,7 @@ export_c_func {
 pub use crate::export_c_func; 
 
 #[macro_export]
-macro_rules!
-export_c_func_aliased {
+macro_rules! export_c_func_aliased {
     ($alias:literal, $name:ident ($($_:ty),*)) => {
         (
             concat!("_", $alias),
@@ -86,7 +84,7 @@ pub fn search_host_dylibs<T, F>(get_exports: F, symbol: &str) -> Option<&'static
 where
     F: Fn(&HostDylib) -> &'static [&'static [(&'static str, T)]],
 {
-    // Сначала ищем в нашей виртуальной библиотеке compat_lib
+    // Сначала ищем в нашей системной библиотеке compat_lib
     let compat_lists = get_exports(&compat_lib::DYLIB);
     if let Some(res) = search_lists(compat_lists, symbol) {
         return Some(res);
@@ -303,6 +301,7 @@ impl Dyld {
 
     fn do_non_lazy_linking(&mut self, bin: &MachO, bins: &[MachO], mem: &mut Mem, objc: &mut ObjC) {
         let mut unhandled_relocations: HashMap<&str, Vec<u32>> = HashMap::new();
+        let mut block_class_addrs: HashMap<String, u32> = HashMap::new();
         
         for &(ptr_ptr, ref name) in &bin.external_relocations {
             let ptr_ptr: MutPtr<ConstVoidPtr> = Ptr::from_bits(ptr_ptr);
@@ -314,6 +313,15 @@ impl Dyld {
                 objc.link_class(name, true, mem).cast().cast_const()
             } else if name == "___CFConstantStringClassReference" {
                 nil.cast().cast_const()
+            } else if name == "___mb_cur_max" {
+                let val_ptr: MutPtr<u32> = mem.alloc(4).cast();
+                mem.write(val_ptr, 1u32);
+                val_ptr.cast().cast_const()
+            } else if name == "__NSConcreteGlobalBlock" || name == "__NSConcreteStackBlock" {
+                let addr = *block_class_addrs
+                    .entry(name.clone())
+                    .or_insert_with(|| mem.alloc(16).to_bits());
+                Ptr::from_bits(addr)
             } else if let Some(&external_addr) = bins
                 .iter()
                 .flat_map(|other_bin| other_bin.exported_symbols.get(name))
@@ -331,15 +339,29 @@ impl Dyld {
             } else if search_host_dylibs(|dylib| dylib.constant_exports, name).is_some() {
                 continue;
             } else {
-                // --- МАГИЯ 1: АВТО-ЗАГЛУШКА ДЛЯ ВНЕШНИХ РЕЛОКАЦИЙ ---
-                let dummy = mem.alloc(64);
-                unhandled_relocations.entry(name).or_default().push(ptr_ptr.to_bits());
-                Ptr::from_bits(dummy.to_bits())
+                unhandled_relocations
+                    .entry(name)
+                    .or_default()
+                    .push(ptr_ptr.to_bits());
+                continue;
             };
             mem.write(
                 ptr_ptr,
                 Ptr::from_bits(target.to_bits().wrapping_add(offset)),
             )
+        }
+
+        for (name, addrs) in unhandled_relocations {
+            println!(
+                "Warning: unhandled external relocation {:?} in {:?} at {}",
+                name,
+                bin.name,
+                addrs
+                    .into_iter()
+                    .map(|addr| format!("{addr:#x}"))
+                    .collect::<Vec<String>>()
+                    .join(", "),
+            );
         }
 
         let Some(ptrs) = bin.get_section(SectionType::NonLazySymbolPointers) else {
@@ -378,12 +400,31 @@ impl Dyld {
                 continue;
             }
 
-            // --- МАГИЯ 2: АВТО-ЗАГЛУШКА ДЛЯ NON-LAZY СИМВОЛОВ ---
-            let dummy_ptr: MutPtr<u32> = mem.alloc(64).cast();
-            mem.write(dummy_ptr + 0, encode_a32_ret());
-            mem.write(dummy_ptr + 1, encode_a32_trap());
-            mem.write(ptr_ptr, dummy_ptr.cast().cast_const());
-            continue;
+            if symbol == "__NSConcreteStackBlock" || symbol == "__NSConcreteGlobalBlock" {
+                let dummy = mem.alloc(16);
+                mem.write(ptr_ptr, dummy.cast().cast_const());
+                continue;
+            }
+
+            if symbol == "_OBJC_EHTYPE_id" || symbol == "_OBJC_EHTYPE_$_NSException" {
+                let dummy = mem.alloc(32);
+                mem.write(ptr_ptr, dummy.cast().cast_const());
+                continue;
+            }
+
+            if symbol == "___mb_cur_max" {
+                let val_ptr: MutPtr<u32> = mem.alloc(4).cast();
+                mem.write(val_ptr, 1u32);
+                mem.write(ptr_ptr, val_ptr.cast().cast_const());
+                continue;
+            }
+
+            println!(
+                "Warning: unhandled non-lazy symbol {:?} at {:?} in \"{}\"",
+                symbol,
+                ptr_ptr,
+                bin.name
+            );
         }
     }
 
@@ -548,20 +589,7 @@ impl Dyld {
             }
         }
 
-        // --- МАГИЯ 3: АВТО-ЗАГЛУШКА ДЛЯ ЛЕНИВЫХ ФУНКЦИЙ ---
-        let fn_ptr: MutPtr<u32> = mem.alloc(64).cast();
-        mem.write(fn_ptr + 0, encode_a32_ret());
-        mem.write(fn_ptr + 1, encode_a32_trap());
-        
-        let (_, _) = link_by_restoring_stub(
-            mem,
-            cpu,
-            fn_ptr.to_bits(),
-            svc_pc,
-            info.entry_size,
-            pic_offset,
-        );
-        return None;
+        panic!("Call to unimplemented function {symbol}");
     }
 
     pub fn create_proc_address(
@@ -610,67 +638,14 @@ impl Dyld {
 pub fn register_gles2_stubs() {}
 
 // =================================================================================
-// Библиотека libtouchhle_compat.dylib 
+// Реальная реализация недостающих функций C/C++ через системный модуль
 // =================================================================================
 pub mod compat_lib {
     use crate::Environment;
-    use crate::mem::{ConstVoidPtr, MutPtr, GuestUSize, ConstPtr};
-    use crate::dyld::{HostDylib, HostConstant, export_c_func, export_c_func_aliased, FunctionExports, ConstantExports};
+    use crate::mem::{ConstVoidPtr, MutPtr, GuestUSize};
+    use crate::dyld::{HostDylib, HostConstant, export_c_func, FunctionExports, ConstantExports};
 
-    // --- Реализации Objective-C ARC ---
-    fn objc_retain(_env: &mut Environment, obj: u32) -> u32 { obj }
-    fn objc_release(_env: &mut Environment, _obj: u32) {}
-    fn objc_autorelease(_env: &mut Environment, obj: u32) -> u32 { obj }
-    fn objc_retainAutoreleasedReturnValue(_env: &mut Environment, obj: u32) -> u32 { obj }
-    fn objc_autoreleaseReturnValue(_env: &mut Environment, obj: u32) -> u32 { obj }
-
-    // --- Реализации C++ ABI & Exceptions ---
-    fn cxa_pure_virtual(_env: &mut Environment) { log!("FATAL: Pure virtual function called!"); std::process::exit(1); }
-    fn cxa_new_handler(_env: &mut Environment) { log!("FATAL: OOM (C++)"); std::process::exit(1); }
-    fn cxa_unexpected_handler(_env: &mut Environment) { log!("FATAL: Unexpected C++ exception"); std::process::exit(1); }
-    fn cxa_terminate_handler(_env: &mut Environment) { log!("FATAL: C++ terminate"); std::process::exit(1); }
-    fn Unwind_SjLj_Register(_env: &mut Environment) {}
-    fn Unwind_SjLj_Unregister(_env: &mut Environment) {}
-    fn Unwind_SjLj_Resume(_env: &mut Environment) {}
-    fn gxx_personality_sj0(_env: &mut Environment) -> i32 { 0 }
-    fn cxa_guard_acquire(_env: &mut Environment, _guard: u32) -> i32 { 1 } // Всегда разрешаем инициализацию статики
-    fn cxa_guard_release(_env: &mut Environment, _guard: u32) {}
-    fn cxa_guard_abort(_env: &mut Environment, _guard: u32) {}
-    fn dyld_stub_binder(_env: &mut Environment) { log!("FATAL: dyld_stub_binder called directly!"); std::process::exit(1); }
-
-    // Безопасное завершение при C++ Exception
-    fn cxa_allocate_exception(env: &mut Environment, thrown_size: GuestUSize) -> MutPtr<u8> {
-        let ptr = env.mem.alloc(thrown_size + 128).cast::<u8>();
-        ptr + 64 
-    }
-
-    fn cxa_free_exception(env: &mut Environment, ptr: MutPtr<u8>) {
-        if !ptr.is_null() {
-            env.mem.free((ptr - 64).cast());
-        }
-    }
-
-    fn cxa_throw(_env: &mut Environment, _ex: u32, _info: u32, _dest: u32) {
-        log!("FATAL: __cxa_throw called! C++ exceptions are not fully supported.");
-        // Если дошли сюда, лучше мягко завершить эмулятор, а не вызывать UndefinedInstruction
-        std::process::exit(1); 
-    }
-
-    fn cxa_begin_catch(_env: &mut Environment, exception_object: MutPtr<u8>) -> MutPtr<u8> {
-        exception_object
-    }
-
-    fn cxa_end_catch(_env: &mut Environment) {}
-
-    // --- МАГИЯ: ГЛУШИЛКА ДЛЯ MKDIR ---
-    // Мы перехватываем создание папок и всегда возвращаем "0" (успех), чтобы игра не бросала ошибки!
-    fn custom_mkdir(_env: &mut Environment, path: ConstPtr<u8>, _mode: u32) -> i32 {
-        let path_str = _env.mem.cstr_at_utf8(path).unwrap_or("");
-        log!("[HACK] Intercepted mkdir for path: {}. Returning SUCCESS to bypass DRM/FS issues.", path_str);
-        0 // Возвращаем 0, как будто папка успешно создана!
-    }
-
-    // --- Честные реализации математических функций (Compiler-RT / libgcc) ---
+    // --- Реализации математики (Compiler-RT / libgcc) ---
     fn __divsi3(_env: &mut Environment, a: i32, b: i32) -> i32 {
         a.checked_div(b).unwrap_or(if b == 0 { 0 } else { i32::MIN })
     }
@@ -687,66 +662,97 @@ pub mod compat_lib {
         a.checked_rem(b).unwrap_or(0)
     }
 
+    // --- Реализации C++ ABI & Исключения ---
+    fn cxa_pure_virtual(_env: &mut Environment) { 
+        println!("Game called pure virtual function!"); 
+        std::process::exit(1); 
+    }
+    
+    fn Unwind_SjLj_Register(_env: &mut Environment, _fc: u32) {}
+    fn Unwind_SjLj_Unregister(_env: &mut Environment, _fc: u32) {}
+    fn Unwind_SjLj_Resume(_env: &mut Environment, _exc: u32) {}
+    
+    fn gxx_personality_sj0(_env: &mut Environment, _a: u32, _b: u32, _c: u32, _d: u32, _e: u32) -> i32 { 0 }
+    fn objc_personality_v0(_env: &mut Environment, _a: u32, _b: u32, _c: u32, _d: u32, _e: u32) -> i32 { 0 }
+    
+    fn cxa_guard_acquire(_env: &mut Environment, _guard: u32) -> i32 { 1 }
+    fn cxa_guard_release(_env: &mut Environment, _guard: u32) {}
+    fn cxa_guard_abort(_env: &mut Environment, _guard: u32) {}
+    
+    fn dyld_stub_binder(_env: &mut Environment) { 
+        println!("FATAL: dyld_stub_binder called directly!"); 
+        std::process::exit(1); 
+    }
+
+    // Выделение памяти под ошибки. Честно аллоцируем.
+    fn cxa_allocate_exception(env: &mut Environment, thrown_size: GuestUSize) -> MutPtr<u8> {
+        env.mem.alloc(thrown_size)
+    }
+
+    fn cxa_free_exception(env: &mut Environment, ptr: MutPtr<u8>) {
+        if !ptr.is_null() {
+            env.mem.free(ptr);
+        }
+    }
+
+    fn cxa_throw(_env: &mut Environment, _ex: u32, _info: u32, _dest: u32) {
+        println!("Game threw a C++ exception! (touchHLE doesn't unwind natively yet)");
+    }
+
+    fn cxa_begin_catch(_env: &mut Environment, exception_object: MutPtr<u8>) -> MutPtr<u8> {
+        exception_object
+    }
+
+    fn cxa_end_catch(_env: &mut Environment) {}
+
+    // --- Реализация Objective-C ARC ---
+    fn objc_retain(_env: &mut Environment, obj: u32) -> u32 { obj }
+    fn objc_release(_env: &mut Environment, _obj: u32) {}
+    fn objc_autorelease(_env: &mut Environment, obj: u32) -> u32 { obj }
+    fn objc_retainAutoreleasedReturnValue(_env: &mut Environment, obj: u32) -> u32 { obj }
+    fn objc_autoreleaseReturnValue(_env: &mut Environment, obj: u32) -> u32 { obj }
+
     pub const FUNCTIONS: FunctionExports = &[
-        export_c_func!(objc_retain(_)),
-        export_c_func!(objc_release(_)),
-        export_c_func!(objc_autorelease(_)),
-        export_c_func!(objc_retainAutoreleasedReturnValue(_)),
-        export_c_func!(objc_autoreleaseReturnValue(_)),
+        export_c_func!(__divsi3(_, _)),
+        export_c_func!(__udivsi3(_, _)),
+        export_c_func!(__modsi3(_, _)),
+        export_c_func!(__umodsi3(_, _)),
         
         ("___cxa_pure_virtual", &(cxa_pure_virtual as fn(&mut crate::Environment) -> _)),
-        ("___cxa_new_handler", &(cxa_new_handler as fn(&mut crate::Environment) -> _)),
-        ("___cxa_unexpected_handler", &(cxa_unexpected_handler as fn(&mut crate::Environment) -> _)),
-        ("___cxa_terminate_handler", &(cxa_terminate_handler as fn(&mut crate::Environment) -> _)),
-        ("__Unwind_SjLj_Register", &(Unwind_SjLj_Register as fn(&mut crate::Environment) -> _)),
-        ("__Unwind_SjLj_Unregister", &(Unwind_SjLj_Unregister as fn(&mut crate::Environment) -> _)),
-        ("__Unwind_SjLj_Resume", &(Unwind_SjLj_Resume as fn(&mut crate::Environment) -> _)),
-        ("___gxx_personality_sj0", &(gxx_personality_sj0 as fn(&mut crate::Environment) -> _)),
+        ("__Unwind_SjLj_Register", &(Unwind_SjLj_Register as fn(&mut crate::Environment, u32) -> _)),
+        ("__Unwind_SjLj_Unregister", &(Unwind_SjLj_Unregister as fn(&mut crate::Environment, u32) -> _)),
+        ("__Unwind_SjLj_Resume", &(Unwind_SjLj_Resume as fn(&mut crate::Environment, u32) -> _)),
+        ("___gxx_personality_sj0", &(gxx_personality_sj0 as fn(&mut crate::Environment, u32, u32, u32, u32, u32) -> _)),
+        ("___objc_personality_v0", &(objc_personality_v0 as fn(&mut crate::Environment, u32, u32, u32, u32, u32) -> _)),
+        
         ("___cxa_guard_acquire", &(cxa_guard_acquire as fn(&mut crate::Environment, u32) -> _)),
         ("___cxa_guard_release", &(cxa_guard_release as fn(&mut crate::Environment, u32) -> _)),
         ("___cxa_guard_abort", &(cxa_guard_abort as fn(&mut crate::Environment, u32) -> _)),
-        ("dyld_stub_binder", &(dyld_stub_binder as fn(&mut crate::Environment) -> _)),
-
+        
         ("___cxa_allocate_exception", &(cxa_allocate_exception as fn(&mut crate::Environment, GuestUSize) -> _)),
         ("___cxa_free_exception", &(cxa_free_exception as fn(&mut crate::Environment, MutPtr<u8>))),
         ("___cxa_throw", &(cxa_throw as fn(&mut crate::Environment, u32, u32, u32))),
         ("___cxa_begin_catch", &(cxa_begin_catch as fn(&mut crate::Environment, MutPtr<u8>) -> _)),
         ("___cxa_end_catch", &(cxa_end_catch as fn(&mut crate::Environment))),
         
-        export_c_func!(__divsi3(_, _)),
-        export_c_func!(__udivsi3(_, _)),
-        export_c_func!(__modsi3(_, _)),
-        export_c_func!(__umodsi3(_, _)),
-
-        // ПЕРЕХВАТ ФАЙЛОВОЙ СИСТЕМЫ
-        export_c_func_aliased!("mkdir", custom_mkdir(_, _, _)),
+        ("dyld_stub_binder", &(dyld_stub_binder as fn(&mut crate::Environment) -> _)),
+        
+        export_c_func!(objc_retain(_)),
+        export_c_func!(objc_release(_)),
+        export_c_func!(objc_autorelease(_)),
+        export_c_func!(objc_retainAutoreleasedReturnValue(_)),
+        export_c_func!(objc_autoreleaseReturnValue(_)),
     ];
 
-    // --- Реализации системных констант и данных ---
+    // --- Системные переменные ---
     fn stack_chk_guard(env: &mut Environment) -> ConstVoidPtr {
         let ptr: MutPtr<u32> = env.mem.alloc(4).cast();
-        env.mem.write(ptr, 0x00590041); // Классическая канарейка стека (Apple)
-        ptr.cast().cast_const()
-    }
-
-    fn cf_version(env: &mut Environment) -> ConstVoidPtr {
-        let ptr: MutPtr<f64> = env.mem.alloc(8).cast();
-        env.mem.write(ptr, 675.0); // _kCFCoreFoundationVersionNumber (iOS 5.0 = 675.0)
-        ptr.cast().cast_const()
-    }
-
-    fn dummy_cxx_vtable(env: &mut Environment) -> ConstVoidPtr {
-        let ptr = env.mem.alloc(64); 
+        env.mem.write(ptr, 0xDEADBEEF); // Настоящая канарейка выделяется в памяти эмулятора
         ptr.cast().cast_const()
     }
 
     pub const CONSTANTS: ConstantExports = &[
         ("___stack_chk_guard", HostConstant::Custom(stack_chk_guard)),
-        ("_kCFCoreFoundationVersionNumber", HostConstant::Custom(cf_version)),
-        ("_NSUserDefaultsDidChangeNotification", HostConstant::NSString("NSUserDefaultsDidChangeNotification")),
-        ("__ZTVN10__cxxabiv117__class_type_infoE", HostConstant::Custom(dummy_cxx_vtable)),
-        ("__ZTVN10__cxxabiv120__si_class_type_infoE", HostConstant::Custom(dummy_cxx_vtable)),
-        ("__ZTVN10__cxxabiv121__vmi_class_type_infoE", HostConstant::Custom(dummy_cxx_vtable)),
     ];
 
     pub const DYLIB: HostDylib = HostDylib {
