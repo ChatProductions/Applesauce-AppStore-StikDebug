698a39d9681fe0ec9c26f2425e16df6164921089
whyineedname <emssaoig1@gmail.com>
2026-04-17 11:52:38 +0300

    Update facebook.rs

--- src/frameworks/facebook.rs
+++ src/frameworks/facebook.rs
@@ -11,34 +11,33 @@ use crate::objc::{objc_classes, ClassExports};
 pub mod fb_classes {
     use super::*;
 
-    pub const CLASSES: ClassExports = &objc_classes![
-        // Основной класс сессии. Возвращаем 1 (YES) на resume, чтобы игра 
-        // думала, что сессия активна.
+    // В твоей версии макрос требует (env, this, _cmd); перед списком классов
+    pub const CLASSES: ClassExports = &objc_classes!(
+        (env, this, _cmd); 
+
         FBSession : NSObject {
-            "-" => "resume" => |_env, _self: MutVoidPtr| -> u8 { 
+            "-" => "resume" => |env, this| -> u8 { 
                 crate::log_dbg!("FBSession resume called");
                 1 
             },
-            "-" => "isConnected" => |_env, _self: MutVoidPtr| -> u8 { 0 },
-            "-" => "logout" => |_env, _self: MutVoidPtr| {},
+            "-" => "isConnected" => |env, this| -> u8 { 0 },
+            "-" => "logout" => |env, this| {},
         },
         
-        // Классы сетевых запросов и парсинга
         FBRequest : NSObject {
-            "-" => "connect" => |_env, _self: MutVoidPtr| {
-                crate::log_dbg!("FBRequest connect called and ignored (no network)");
+            "-" => "connect" => |env, this| {
+                crate::log_dbg!("FBRequest connect called");
             },
         },
         
         FBXMLHandler : NSObject {},
         
-        // Диалоговые окна наследуются от UIView
         FBDialog : UIView {
-            "-" => "show" => |_env, _self: MutVoidPtr| {
+            "-" => "show" => |env, this| {
                 crate::log_dbg!("FBDialog show called");
             },
-            "-" => "dismissWithSuccess:animated:" => |_env, _self: MutVoidPtr, _success: u8, _animated: u8| {
-                crate::log_dbg!("FBDialog dismissWithSuccess:animated: called");
+            "-" => "dismissWithSuccess:animated:" => |env, this, _success: u8, _animated: u8| {
+                crate::log_dbg!("FBDialog dismiss called");
             },
         },
         
@@ -46,9 +45,8 @@ pub mod fb_classes {
         FBLoginDialog : FBDialog {},
         FBPermissionDialog : FBDialog {},
         
-        // Кнопка наследуется от UIButton
         FBLoginButton : UIButton {}
-    ];
+    );
 }
 
 pub const DYLIB: HostDylib = HostDylib {
