906fdcf89be55d14df37de570ac6306e2f740bf7
masterfanaedoodledash-cmd <masterfanaedoodledash@gmail.com>
2026-04-17 02:50:42 +0200

    Update libc.rs

--- src/libc.rs
+++ src/libc.rs
@@ -18,6 +18,7 @@ pub mod crypto;
 pub mod ctype;
 pub mod cxxabi;
 pub mod dirent;
+pub mod dispatch;
 pub mod dlfcn;
 pub mod dns_sd;
 pub mod errno;
@@ -52,7 +53,7 @@ pub const DYLIB: crate::dyld::HostDylib = crate::dyld::HostDylib {
     path: "/usr/lib/libSystem.B.dylib",
     aliases: &["/usr/lib/libSystem.dylib"],
     class_exports: &[],
-    constant_exports: &[ctype::CONSTANTS, stdio::CONSTANTS, mach::init::CONSTANTS, ssp::CONSTANTS],
+    constant_exports: &[ctype::CONSTANTS, dispatch::CONSTANTS, stdio::CONSTANTS, mach::init::CONSTANTS, ssp::CONSTANTS],
     function_exports: &[
         arpa::inet::FUNCTIONS,
         clocale::FUNCTIONS,
@@ -60,6 +61,7 @@ pub const DYLIB: crate::dyld::HostDylib = crate::dyld::HostDylib {
         cxxabi::FUNCTIONS,
         crypto::FUNCTIONS,
         dirent::FUNCTIONS,
+        dispatch::FUNCTIONS,
         dlfcn::FUNCTIONS,
         dns_sd::FUNCTIONS,
         errno::FUNCTIONS,
