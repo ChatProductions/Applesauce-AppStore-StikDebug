f86ca8956ce1460482d829ccbaf9c2345915056a
j92580498-max <j92580498@gmail.com>
2026-04-19 02:33:18 +0300

    Update symphonia_formats.rs

--- src/audio/symphonia_formats.rs
+++ src/audio/symphonia_formats.rs
@@ -4,19 +4,12 @@
  * If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
-//! Quick-and-dirty decoding of miscellaneous formats (MP3, AAC) to linear PCM.
+//! Quick-and-dirty decoding of miscellaneous formats (MP3, AAC, CAF) to linear PCM.
 //!
 //! This should be the only module in touchHLE that makes use of [symphonia].
-//!
-//! For AAC, Only the LC profile and MPEG-4 container format are supported (see
-//! feature list in Cargo.toml).
 
 use std::io::Cursor;
 use symphonia::core::audio::AudioSpec;
-use symphonia::core::codecs::audio::well_known::{
-    CODEC_ID_AAC, CODEC_ID_ADPCM_IMA_QT, CODEC_ID_ADPCM_IMA_WAV, CODEC_ID_ALAC, CODEC_ID_MP3,
-    CODEC_ID_PCM_S16LE,
-};
 use symphonia::core::io::MediaSourceStream;
 
 /// PCM data decoded from an miscellaneous format file.
@@ -33,45 +26,45 @@ pub struct SymphoniaDecodedToPcm {
 pub fn decode_symphonia_to_pcm(file: Cursor<Vec<u8>>) -> Result<SymphoniaDecodedToPcm, ()> {
     let mss = MediaSourceStream::new(Box::new(file), Default::default());
 
-    // If this failed, the container format is not supported.
-    let mut probed = symphonia::default::get_probe()
-        .probe(
-            &Default::default(),
-            mss,
-            Default::default(),
-            Default::default(),
-        )
-        .map_err(|_| ())?;
+    // Пробуем определить формат. Если не вышло - логируем реальную причину.
+    let mut probed = match symphonia::default::get_probe()
+        .probe(&Default::default(), mss, Default::default(), Default::default()) {
+        Ok(p) => p,
+        Err(e) => {
+            log!("Symphonia probe failed: {:?}", e);
+            return Err(());
+        }
+    };
 
+    // Находим любую аудиодорожку, которую Symphonia способна прочитать,
+    // без жесткого ограничения по CODEC_ID.
     let track = probed
         .tracks()
         .iter()
         .find(|t| {
             if let Some(codec_params) = &t.codec_params {
-                if let Some(audio_codec_params) = codec_params.audio() {
-                    audio_codec_params.codec == CODEC_ID_AAC
-                        || audio_codec_params.codec == CODEC_ID_ADPCM_IMA_WAV
-                        || audio_codec_params.codec == CODEC_ID_ADPCM_IMA_QT
-                        || audio_codec_params.codec == CODEC_ID_ALAC
-                        || audio_codec_params.codec == CODEC_ID_MP3
-                        || audio_codec_params.codec == CODEC_ID_PCM_S16LE
-                } else {
-                    false
-                }
+                codec_params.codec != symphonia::core::codecs::CODEC_ID_NULL
             } else {
                 false
             }
         })
-        .ok_or(())?;
+        .ok_or_else(|| {
+            log!("Symphonia: No supported audio tracks found in file");
+        })
+        .ok()?;
 
     let track_id = track.id;
+    let audio_codec_params = track.codec_params.clone();
 
-    // Not sure why this would fail, maybe an unusual AAC track.
-    let audio_codec_params = track.codec_params.as_ref().unwrap().audio().unwrap();
-
-    let mut decoder = symphonia::default::get_codecs()
-        .make_audio_decoder(audio_codec_params, &Default::default())
-        .map_err(|_| ())?;
+    // Создаем декодер
+    let mut decoder = match symphonia::default::get_codecs()
+        .make_audio_decoder(&audio_codec_params, &Default::default()) {
+        Ok(d) => d,
+        Err(e) => {
+            log!("Symphonia failed to create audio decoder: {:?}", e);
+            return Err(());
+        }
+    };
 
     let mut out_pcm = Vec::<u8>::new();
     let mut audio_spec: Option<AudioSpec> = None;
@@ -81,36 +74,34 @@ pub fn decode_symphonia_to_pcm(file: Cursor<Vec<u8>>) -> Result<SymphoniaDecoded
 
         loop {
             let packet = match probed.next_packet() {
-                Ok(packet) => match packet {
-                    Some(packet) => packet,
-                    // "If Ok(None) is returned, the media has ended and
-                    // no more packets will be produced until the reader
-                    // is seeked to a new position."
-                    None => break,
-                },
-                // Assume I/O errors can only mean end-of-file, because the
-                // entire file is in-memory.
+                Ok(packet) => packet,
+                // Конец файла
                 Err(symphonia::core::errors::Error::IoError(_)) => break,
-                Err(_) => return Err(()),
+                Err(e) => {
+                    log!("Symphonia packet read error: {:?} (Stopping read but keeping decoded audio)", e);
+                    break;
+                }
             };
 
             if packet.track_id() != track_id {
                 continue;
             }
 
-            let Ok(decoded_packet) = decoder.decode(&packet) else {
-                break;
+            let decoded_packet = match decoder.decode(&packet) {
+                Ok(p) => p,
+                Err(symphonia::core::errors::Error::DecodeError(e)) => {
+                    // Ошибки декодирования (битый фрейм MP3/AAC) можно игнорировать и идти дальше
+                    log!("Symphonia decode error (recoverable): {:?}", e);
+                    continue;
+                }
+                Err(e) => {
+                    log!("Symphonia fatal decode error: {:?}", e);
+                    break;
+                }
             };
 
-            // For some reason, the "audio spec" (number of channels etc)
-            // is reported per-packet? This is weird because it must be the same
-            // for all of them.
             let audio_spec = audio_spec.get_or_insert_with(|| decoded_packet.spec().clone());
-            assert_eq!(audio_spec, decoded_packet.spec());
-
-            // Note that this assumes every packet's buffer's capacity is the
-            // same, which is a dubious assumption, but Symphonia's own example
-            // code does it, so maybe it's fine?
+            
             let tmp_raw_s16_buf = tmp_raw_s16_buf
                 .get_or_insert_with(|| Vec::with_capacity(decoded_packet.capacity()));
 
@@ -121,7 +112,9 @@ pub fn decode_symphonia_to_pcm(file: Cursor<Vec<u8>>) -> Result<SymphoniaDecoded
         }
     }
 
-    let audio_spec = audio_spec.ok_or(())?;
+    let audio_spec = audio_spec.ok_or_else(|| {
+        log!("Symphonia: File yielded no valid audio data");
+    }).ok()?;
 
     Ok(SymphoniaDecodedToPcm {
         bytes: out_pcm,
