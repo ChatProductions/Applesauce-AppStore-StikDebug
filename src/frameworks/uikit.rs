7c502be30e8c8a6a4122f45b4c6ac4f1df07ab35
j92580498-max <j92580498@gmail.com>
2026-04-18 12:48:18 +0300

    Update uikit.rs

--- src/frameworks/uikit.rs
+++ src/frameworks/uikit.rs
@@ -164,27 +164,14 @@ pub fn handle_events(env: &mut Environment) -> Option<Instant> {
                 ui_touch::handle_event(env, event)
             }
             Event::AppWillResignActive => {
-                // Getting this event means touchHLE is becoming inactive, e.g.
-                // due to switching apps. The obvious way to handle this would
-                // be to just send `applicationWillResignActive:` to the
-                // UIApplicationDelegate. However:
-                // - touchHLE's event loop can't handle an inactive app well
-                //   right now. For example, audio isn't paused.
-                // - touchHLE's event loop can't handle the subsequent
-                //   termination of an app right now: it doesn't manage to send
-                //   the `applicationWillTerminate:` message in time. This can
-                //   mean loss of data!
-                // Therefore, for the moment we will simulate the early iOS
-                // behavior where switching app usually resulted in termination.
-                // We can usually handle this in time, so there won't be data
-                // loss, nor problems with background resource usage or audio.
-                // TODO: Handle this better.
-                log!("Handling app-will-resign-active event: exiting.");
-                ui_application::exit(env);
+                // SUPER HACK: Полностью игнорируем сворачивание/потерю фокуса. 
+                // Это предотвратит отправку сигнала в игру и спасет от краша 
+                // в движке анимаций (core_animation/composition.rs).
+                log!("SUPER HACK: Completely ignoring AppWillResignActive to prevent composition panic.");
             }
             Event::AppWillTerminate => {
-                log!("Handling app-will-terminate event.");
-                ui_application::exit(env);
+                // Аналогично глушим и этот сигнал
+                log!("SUPER HACK: Completely ignoring AppWillTerminate.");
             }
             Event::EnterDebugger => {
                 if env.is_debugging_enabled() {
