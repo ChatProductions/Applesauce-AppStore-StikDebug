fca5be47eef1c0f012bad5317e19869d3f3b5e04
j92580498-max <j92580498@gmail.com>
2026-04-19 17:06:39 +0300

    Update ui_responder.rs

--- src/frameworks/uikit/ui_responder.rs
+++ src/frameworks/uikit/ui_responder.rs
@@ -6,10 +6,11 @@
 //! `UIResponder`.
 
 use crate::objc::{id, msg, nil, objc_classes, ClassExports};
+use crate::Environment;
 
 #[derive(Default)]
 pub struct State {
-    pub(crate) first_responder: id,
+    pub first_responder: id,
 }
 
 pub const CLASSES: ClassExports = objc_classes! {
@@ -70,20 +71,26 @@ pub const CLASSES: ClassExports = objc_classes! {
 }
 
 - (bool)isFirstResponder {
-    false
+    env.framework_state.uikit.ui_responder.first_responder == this
 }
+
 - (bool)canBecomeFirstResponder {
     false
 }
+
 - (bool)becomeFirstResponder {
-    // TODO
-    false
+    env.framework_state.uikit.ui_responder.first_responder = this;
+    true
 }
+
 - (bool)canResignFirstResponder {
     true
 }
+
 - (bool)resignFirstResponder {
-    // TODO
+    if env.framework_state.uikit.ui_responder.first_responder == this {
+        env.framework_state.uikit.ui_responder.first_responder = nil;
+    }
     true
 }
 
