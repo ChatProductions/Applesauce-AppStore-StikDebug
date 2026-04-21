993c48d8acd42c6d9ba622e03fc0d14fbf9dfe30
TimofeyLednev <132895841+TimofeyLednev@users.noreply.github.com>
2026-04-21 15:26:08 +0500

    The previous code changes didn't work, but these code changes should work

--- src/frameworks/uikit.rs
+++ src/frameworks/uikit.rs
@@ -222,12 +222,27 @@ pub fn handle_events(env: &mut Environment) -> Option<Instant> {
                 ui_touch::handle_event(env, event)
             }
             Event::AppWillResignActive => {
-                // SUPER HACK: Fully ignore focus loss to prevent crashes in the
-                // animation composition layer (core_animation/composition.rs).
-                log!("SUPER HACK: Ignoring AppWillResignActive to prevent composition panic.");
+                // Getting this event means touchHLE is becoming inactive, e.g.
+                // due to switching apps. The obvious way to handle this would
+                // be to just send `applicationWillResignActive:` to the
+                // UIApplicationDelegate. However:
+                // - touchHLE's event loop can't handle an inactive app well
+                //   right now. For example, audio isn't paused.
+                // - touchHLE's event loop can't handle the subsequent
+                //   termination of an app right now: it doesn't manage to send
+                //   the `applicationWillTerminate:` message in time. This can
+                //   mean loss of data!
+                // Therefore, for the moment we will simulate the early iOS
+                // behavior where switching app usually resulted in termination.
+                // We can usually handle this in time, so there won't be data
+                // loss, nor problems with background resource usage or audio.
+                // TODO: Handle this better.
+                log!("Handling app-will-resign-active event: exiting.");
+                ui_application::exit(env);
             }
             Event::AppWillTerminate => {
-                log!("SUPER HACK: Ignoring AppWillTerminate.");
+                log!("Handling app-will-terminate event.");
+                ui_application::exit(env);
             }
             Event::EnterDebugger => {
                 if env.is_debugging_enabled() {
