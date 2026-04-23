ce5eb1abef2ffe5d64b86cc44742e65579eec875
j92580498-max <j92580498@gmail.com>
2026-04-23 23:10:35 +0300

    Refactor Core Foundation to use Foundation framework

--- src/frameworks/core_foundation.rs
+++ src/frameworks/core_foundation.rs
@@ -1,114 +1,413 @@
 /*
  * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * License, v. 2.0.
+ * If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
-//! The Core Foundation framework.
 //!
-//! In Apple's implementation, this is a layer independent of, or below,
-//! Foundation, and there is "Toll-Free Bridging" that lets some Foundation
-//! types be used as if they were the corresponding Core Foundation types and
-//! vice-versa. But in this implementation we will cheat and implement things
-//! backwards (Core Foundation on top of Foundation) where we can get away with
-//! it.
+//! The Foundation framework.
 //!
-//! Useful resources:
-//! - Apple's [Core Foundation Design Concepts](https://developer.apple.com/library/archive/documentation/CoreFoundation/Conceptual/CFDesignConcepts/CFDesignConcepts.html)
-//! - Apple's [Memory Management Programming Guide for Core Foundation](https://developer.apple.com/library/archive/documentation/CoreFoundation/Conceptual/CFMemoryMgmt/CFMemoryMgmt.html)
+//! A concept that Foundation really likes is "class clusters": abstract classes
+//! with private concrete implementations.
+//! Apple has their own explanation of it
+//! in [Cocoa Core Competencies](https://developer.apple.com/library/archive/documentation/General/Conceptual/DevPedia-CocoaCore/ClassCluster.html).
+//!
+//! Being aware of this concept will make common types like `NSArray` and
+//! `NSString` easier to understand.
 
-pub mod cf_allocator;
-pub mod cf_array;
-pub mod cf_bundle;
-pub mod cf_data;
-pub mod cf_dictionary;
-pub mod cf_host;
-pub mod cf_locale;
-pub mod cf_number;
-pub mod cf_preferences;
-pub mod cf_run_loop;
-pub mod cf_run_loop_timer;
-pub mod cf_socket;
-pub mod cf_stream;
-pub mod cf_string;
-pub mod cf_type;
-pub mod cf_url;
-pub mod cf_uuid;
-pub mod time;
+use crate::dyld::{export_c_func, FunctionExports, HostConstant, ConstantExports};
+use crate::frameworks::foundation::ns_string::CFStringGetCharactersPtr;
+use crate::objc::id;
+use crate::Environment;
+use crate::mem::{ConstPtr, MutPtr};
+
+pub mod _nib_archive_decoder;
+pub mod ab_people_picker_navigation_controller;
+pub mod asidentifier_manager;
+pub mod chipmunk_space;
+pub mod ns_array;
+pub mod ns_assertion_handler;
+pub mod ns_autorelease_pool;
+pub mod ns_bundle;
+pub mod ns_calendar;
+pub mod ns_character_set;
+pub mod ns_coder;
+pub mod ns_condition;
+pub mod ns_data;
+pub mod ns_date;
+pub mod ns_date_components;
+pub mod ns_date_formatter;
+pub mod ns_decimal_number;
+pub mod ns_dictionary;
+pub mod ns_enumerator;
+pub mod ns_error;
+pub mod ns_exception;
+pub mod ns_file_handle;
+pub mod ns_file_manager;
+pub mod ns_index_path;
+pub mod ns_input_stream;
+pub mod ns_invocation;
+pub mod ns_keyed_archiver;
+pub mod ns_keyed_unarchiver;
+pub mod ns_locale;
+pub mod ns_lock;
+pub mod ns_log;
+pub mod ns_metadata_query;
+pub mod ns_notification;
+pub mod ns_notification_center;
+pub mod ns_null;
+pub mod ns_number_formatter;
+pub mod ns_objc_runtime;
+pub mod ns_object;
+pub mod ns_operation;
+pub mod ns_persistent_store_coordinator;
+pub mod ns_predicate;
+pub mod ns_process_info;
+pub mod ns_property_list_serialization;
+pub mod ns_run_loop;
+pub mod ns_scanner;
+pub mod ns_set;
+pub mod ns_sort_descriptor;
+pub mod ns_string;
+pub mod ns_thread;
+pub mod ns_time_zone;
+pub mod ns_timer;
+pub mod ns_ubiquitous_key_value_store;
+pub mod ns_undo_manager;
+pub mod ns_url;
+pub mod ns_url_connection;
+pub mod ns_url_request;
+pub mod ns_user_defaults;
+pub mod ns_uuid;
+pub mod ns_value;
+pub mod ns_xml_parser;
+
+pub fn NSGetSizeAndAlignment(
+    env: &mut Environment,
+    type_ptr: ConstPtr<u8>,
+    size_out: MutPtr<NSUInteger>,
+    align_out: MutPtr<NSUInteger>,
+) -> ConstPtr<u8> {
+    let (next_ptr, size, align) = parse_objc_type(env, type_ptr);
+    if !size_out.is_null() {
+        env.mem.write(size_out, size as NSUInteger);
+    }
+    if !align_out.is_null() {
+        env.mem.write(align_out, align as NSUInteger);
+    }
+    
+    next_ptr
+}
+
+fn parse_objc_type(env: &mut Environment, mut ptr: ConstPtr<u8>) -> (ConstPtr<u8>, u32, u32) {
+    // Пропускаем модификаторы типа (const, in, out, inout, bycopy, byref, oneway)
+    loop {
+        let c = env.mem.read(ptr) as char;
+        match c {
+            'r' | 'n' | 'N' |
+            'o' | 'O' | 'R' | 'V' => {
+                ptr = ptr + 1;
+            }
+            _ => break,
+        }
+    }
+
+    let c = env.mem.read(ptr) as char;
+    ptr = ptr + 1;
+
+    match c {
+        // Базовые типы
+        'c' |
+        'C' | 'B' => (ptr, 1, 1),
+        's' |
+        'S' => (ptr, 2, 2),
+        'i' | 'I' | 'l' | 'L' |
+        'f' | 'W' => (ptr, 4, 4),
+        'q' | 'Q' |
+        'd' => (ptr, 8, 8),
+        'v' => (ptr, 0, 1), // void
+        
+        // Указатели, объекты (id), классы (Class), селекторы (SEL), неизвестные указатели (?)
+        '*' |
+        '@' | '#' | ':' | '?' => (ptr, 4, 4),
+        
+        // Указатель на другой тип: размер всегда 4, но нужно "проглотить" тип, на который он указывает
+        '^' => {
+            let (next_ptr, _, _) = parse_objc_type(env, ptr);
+            (next_ptr, 4, 4)
+        }
+        
+        // Массивы: [len+type]
+        '[' => {
+            let mut len = 0;
+            loop {
+                let c = env.mem.read(ptr) as char;
+                if c.is_ascii_digit() {
+                    len = len * 10 + c.to_digit(10).unwrap();
+                    ptr = ptr + 1;
+                } else {
+                    break;
+                }
+            }
+            let (mut next_ptr, elem_size, elem_align) = parse_objc_type(env, ptr);
+            if env.mem.read(next_ptr) as char == ']' {
+                next_ptr = next_ptr + 1;
+            }
+            (next_ptr, len * elem_size, elem_align)
+        }
+        
+        // Структуры: {name=types}
+        '{' => {
+            loop {
+                let c = env.mem.read(ptr) as char;
+                ptr = ptr + 1;
+                if c == '=' ||
+                c == '}' {
+                    if c == '}' { return (ptr, 0, 1);
+                    } // Opaque
+                    break;
+                }
+            }
+            let mut total_size = 0;
+            let mut max_align = 1;
+            loop {
+                let c = env.mem.read(ptr) as char;
+                if c == '}' {
+                    ptr = ptr + 1;
+                    break;
+                }
+                if c == '\0' { break;
+                }
+                
+                // Пропускаем имена полей (например: "x"f)
+                if c == '"' {
+                    ptr = ptr + 1;
+                   
+                    loop {
+                        let nc = env.mem.read(ptr) as char;
+                        ptr = ptr + 1;
+                      
+                        if nc == '"' { break;
+                        }
+                    }
+                } else {
+                    let (next_ptr, elem_size, elem_align) = parse_objc_type(env, ptr);
+                    ptr = next_ptr;
+                    
+                    if elem_align > 0 {
+                        let rem = total_size % elem_align;
+                        if rem != 0 {
+                            total_size += elem_align - rem;
+                        }
+                        if elem_align > max_align {
+                            max_align = elem_align;
+                        }
+                    }
+                    total_size += elem_size;
+                }
+            }
+            if max_align > 0 {
+                let rem = total_size % max_align;
+                if rem != 0 {
+                    total_size += max_align - rem;
+                }
+            }
+            (ptr, total_size, max_align)
+        }
+        
+        // Объединения: (name=types)
+        '(' => {
+            loop {
+                let c = env.mem.read(ptr) as char;
+    
+                ptr = ptr + 1;
+                if c == '=' || c == ')' {
+                    if c == ')' { return (ptr, 0, 1);
+                    }
+                    break;
+                }
+            }
+            let mut max_size = 0;
+            let mut max_align = 1;
+            loop {
+                let c = env.mem.read(ptr) as char;
+                if c == ')' {
+                    ptr = ptr + 1;
+                    break;
+                }
+                if c == '\0' { break;
+                }
+                
+                if c == '"' {
+                    ptr = ptr + 1;
+                    loop {
+                   
+                        let nc = env.mem.read(ptr) as char;
+                        ptr = ptr + 1;
+                        if nc == '"' { break;
+                        }
+                    }
+                } else {
+                    let (next_ptr, elem_size, elem_align) = parse_objc_type(env, ptr);
+                    ptr = next_ptr;
+                    if elem_size > max_size { max_size = elem_size;
+                    }
+                    if elem_align > max_align { max_align = elem_align;
+                    }
+                }
+            }
+            (ptr, max_size, max_align)
+        }
+        
+        // Битовые поля: bNUM
+        'b' => {
+            let mut bits = 0;
+            loop {
+                let c = env.mem.read(ptr) as char;
+                if c.is_ascii_digit() {
+                    bits = bits * 10 + c.to_digit(10).unwrap();
+                    ptr = ptr + 1;
+                } else {
+                    break;
+                }
+            }
+            let bytes = (bits + 7) / 8;
+            (ptr, bytes, 1)
+        }
+        
+        _ => (ptr, 0, 1),
+    }
+}
+                
+pub const STUB_CONSTANTS: ConstantExports = &[
+    ("_NSLocalizedFailureReasonErrorKey", HostConstant::NSString("NSLocalizedFailureReasonErrorKey")),
+    ("_NSURLErrorDomain", HostConstant::NSString("NSURLErrorDomain")),
+    ("_NSKeyValueChangeNewKey", HostConstant::NSString("new")), // ДОБАВЛЕН КЛЮЧ KVO
+    ("_OBJC_IVAR_$_NSObject.isa", HostConstant::U32(0)), // ДОБАВЛЕНА ЗАГЛУШКА NSObject
+];
 
 pub const DYLIB: crate::dyld::HostDylib = crate::dyld::HostDylib {
-    path: "/System/Library/Frameworks/CoreFoundation.framework/CoreFoundation",
+    path: "/System/Library/Frameworks/Foundation.framework/Foundation",
     aliases: &[],
     class_exports: &[
-        cf_run_loop_timer::CLASSES, // Special internal classes.
-        cf_host::CLASSES,
-        cf_stream::CLASSES,
-        cf_uuid::CLASSES,
+        _nib_archive_decoder::CLASSES,
+        ab_people_picker_navigation_controller::CLASSES,
+        asidentifier_manager::CLASSES,
+        chipmunk_space::CLASSES,
+        ns_array::CLASSES,
+        ns_assertion_handler::CLASSES,
+        ns_autorelease_pool::CLASSES,
+        ns_bundle::CLASSES,
+        ns_calendar::CLASSES,
+        ns_character_set::CLASSES,
+        ns_coder::CLASSES,
+        ns_condition::CLASSES,
+        ns_data::CLASSES,
+        ns_date::CLASSES,
+        ns_date_components::CLASSES,
+        ns_date_formatter::CLASSES,
+        ns_decimal_number::CLASSES,
+        ns_dictionary::CLASSES,
+        ns_enumerator::CLASSES,
+        ns_error::CLASSES,
+        ns_exception::CLASSES,
+        ns_file_handle::CLASSES,
+        ns_file_manager::CLASSES,
+        ns_index_path::CLASSES,
+        ns_input_stream::CLASSES,
+        ns_invocation::CLASSES,
+        ns_keyed_archiver::CLASSES,
+        ns_keyed_unarchiver::CLASSES,
+        ns_locale::CLASSES,
+        ns_lock::CLASSES,
+        ns_metadata_query::CLASSES,
+        ns_notification::CLASSES,
+        ns_notification_center::CLASSES,
+        ns_null::CLASSES,
+        ns_number_formatter::CLASSES,
+        ns_object::CLASSES,
+        ns_operation::CLASSES,
+        ns_persistent_store_coordinator::CLASSES,
+        ns_predicate::CLASSES,
+        ns_process_info::CLASSES,
+        ns_property_list_serialization::CLASSES,
+        ns_run_loop::CLASSES,
+        ns_scanner::CLASSES,
+        ns_set::CLASSES,
+        ns_sort_descriptor::CLASSES,
+        ns_string::CLASSES,
+        ns_thread::CLASSES,
+        ns_timer::CLASSES,
+        ns_time_zone::CLASSES,
+        ns_ubiquitous_key_value_store::CLASSES,
+        ns_undo_manager::CLASSES,
+        ns_url::CLASSES,
+        ns_url_connection::CLASSES,
+        ns_url_request::CLASSES,
+        ns_user_defaults::CLASSES,
+        ns_uuid::CLASSES,
+        ns_value::CLASSES,
+        ns_xml_parser::CLASSES,
     ],
     constant_exports: &[
-        cf_allocator::CONSTANTS,
-        cf_bundle::CONSTANTS,
-        cf_dictionary::CONSTANTS,
-        cf_locale::CONSTANTS,
-        cf_number::CONSTANTS,
-        cf_preferences::CONSTANTS,
-        cf_run_loop::CONSTANTS,
-        cf_stream::CONSTANTS,
+        ns_calendar::CONSTANTS,
+        ns_error::CONSTANTS,
+        ns_exception::CONSTANTS,
+        ns_file_manager::CONSTANTS,
+        ns_keyed_unarchiver::CONSTANTS,
+        ns_locale::CONSTANTS,
+        ns_run_loop::CONSTANTS,
+        STUB_CONSTANTS,
     ],
     function_exports: &[
         FUNCTIONS,
-        cf_array::FUNCTIONS,
-        cf_dictionary::FUNCTIONS,
-        cf_bundle::FUNCTIONS,
-        cf_socket::FUNCTIONS,
-        cf_data::FUNCTIONS,
-        cf_host::FUNCTIONS,
-        cf_locale::FUNCTIONS,
-        cf_number::FUNCTIONS,
-        cf_preferences::FUNCTIONS,
-        cf_run_loop::FUNCTIONS,
-        cf_run_loop_timer::FUNCTIONS,
-        cf_string::FUNCTIONS,
-        cf_stream::FUNCTIONS,
-        cf_type::FUNCTIONS,
-        cf_url::FUNCTIONS,
-        cf_uuid::FUNCTIONS,
-        time::FUNCTIONS,
+        ns_exception::FUNCTIONS,
+        ns_file_manager::FUNCTIONS,
+        ns_log::FUNCTIONS,
+        ns_objc_runtime::FUNCTIONS,
     ],
 };
 
-pub use cf_type::{CFRelease, CFRetain, CFTypeRef};
-
-pub type CFHashCode = u32;
-pub type CFIndex = i32;
-pub type CFOptionFlags = u32;
-pub type CFComparisonResult = CFIndex;
-
-use crate::abi::GuestArg;
-use crate::dyld::FunctionExports;
-use crate::environment::Environment;
-use crate::frameworks::foundation::ns_string::to_rust_string;
-use crate::mem::SafeRead;
-use crate::objc::id;
-use crate::{export_c_func, impl_GuestRet_for_large_struct, msg};
-
-pub const kCFNotFound: CFIndex = -1;
-
-#[derive(Copy, Clone, Debug)]
-#[repr(C, packed)]
-pub struct CFRange {
-    pub location: CFIndex,
-    pub length: CFIndex,
+#[derive(Default)]
+pub struct State {
+    ns_autorelease_pool: ns_autorelease_pool::State,
+    ns_bundle: ns_bundle::State,
+    ns_calendar: ns_calendar::State,
+    pub ns_exception: ns_exception::State,
+    ns_file_manager: ns_file_manager::State,
+    ns_locale: ns_locale::State,
+    ns_notification_center: ns_notification_center::State,
+    ns_null: ns_null::State,
+    ns_process_info: ns_process_info::State,
+    ns_run_loop: ns_run_loop::State,
+    ns_string: ns_string::State,
+    ns_thread: ns_thread::State,
+    pub ns_ubiquitous_key_value_store: ns_ubiquitous_key_value_store::State,
+    pub ns_undo_manager: ns_undo_manager::State,
+    ns_user_defaults: ns_user_defaults::State,
 }
 
-unsafe impl SafeRead for CFRange {}
-impl_GuestRet_for_large_struct!(CFRange);
-impl GuestArg for CFRange {
-    const REG_COUNT: usize = 2;
+pub type NSInteger = i32;
+pub type NSUInteger = u32;
 
+// this should be equal to NSIntegerMax
+pub const NSNotFound: i32 = 0x7fffffff;
+
+#[derive(Debug)]
+#[repr(C, packed)]
+pub struct NSRange {
+    pub location: NSUInteger,
+    pub length: NSUInteger,
+}
+unsafe impl crate::mem::SafeRead for NSRange {}
+crate::abi::impl_GuestRet_for_large_struct!(NSRange);
+
+impl crate::abi::GuestArg for NSRange {
+    const REG_COUNT: usize = 2;
     fn from_regs(regs: &[u32]) -> Self {
-        CFRange {
-            location: GuestArg::from_regs(&regs[0..1]),
-            length: GuestArg::from_regs(&regs[1..2]),
+        NSRange {
+            location: crate::abi::GuestArg::from_regs(&regs[0..1]),
+            length: crate::abi::GuestArg::from_regs(&regs[1..2]),
         }
     }
     fn to_regs(self, regs: &mut [u32]) {
@@ -117,14 +416,41 @@ impl GuestArg for CFRange {
     }
 }
 
-fn CFShow(env: &mut Environment, obj: CFTypeRef) {
-    // TODO: support opaque types
-    // TODO: use description callbacks if defined
-    let description: id = msg![env; obj description];
-    // The output should be printed to stderr without any prefix,
-    // but CFShow() is meant to be used for debugging purposes,
-    // so just logging with CF module prefix should be fine too.
-    log!("{}", to_rust_string(env, description));
+fn NSStringFromRange(env: &mut Environment, range: NSRange) -> id {
+    let loc = range.location;
+    let len = range.length;
+    let string = format!("{{{loc}, {len}}}");
+    ns_string::from_rust_string(env, string)
 }
 
-const FUNCTIONS: FunctionExports = &[export_c_func!(CFShow(_))];
+pub type NSComparisonResult = NSInteger;
+pub const NSOrderedAscending: NSComparisonResult = -1;
+pub const NSOrderedSame: NSComparisonResult = 0;
+pub const NSOrderedDescending: NSComparisonResult = 1;
+
+/// Number of seconds.
+pub type NSTimeInterval = f64;
+
+/// UTF-16 code unit.
+#[allow(non_camel_case_types)]
+pub type unichar = u16;
+
+/// Utility to help with implementing the `hash` method, which various classes
+/// in Foundation have to do.
+fn hash_helper<T: std::hash::Hash>(hashable: &T) -> NSUInteger {
+    use std::hash::Hasher;
+    // Rust documentation says DefaultHasher::new() should always return the
+    // same instance, so this should give consistent hashes.
+    let mut hasher = std::collections::hash_map::DefaultHasher::new();
+    hashable.hash(&mut hasher);
+
+    let hash_u64: u64 = hasher.finish();
+    (hash_u64 as u32) ^ ((hash_u64 >> 32) as u32)
+}
+
+const FUNCTIONS: FunctionExports = &[
+    export_c_func!(NSStringFromRange(_)),
+    export_c_func!(NSGetSizeAndAlignment(_, _, _)),
+    export_c_func!(CFStringGetCharactersPtr(_)),
+];
+
