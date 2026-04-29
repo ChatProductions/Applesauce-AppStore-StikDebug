eda20165045b31880a8c1152611ce2993c7c42d3
whyineedname <emssaoig1@gmail.com>
2026-04-29 10:14:27 +0300

    Add host_info function and host_basic_info struct

--- src/libc/mach_o.rs
+++ src/libc/mach_o.rs
@@ -7,6 +7,73 @@
 
 use crate::dyld::{export_c_func, FunctionExports};
 use crate::Environment;
+use crate::mem::{MutPtr, SafeRead};
+
+// --- ДОБАВЛЯЕМ СТРУКТУРЫ И КОНСТАНТЫ ДЛЯ host_info ---
+const HOST_BASIC_INFO: i32 = 1;
+
+#[repr(C)]
+#[derive(Copy, Clone, Default)]
+struct host_basic_info {
+    max_cpus: i32,
+    avail_cpus: i32,
+    memory_size: u32,
+    cpu_type: i32,
+    cpu_subtype: i32,
+    cpu_threadtype: i32,
+    physical_cpu: i32,
+    physical_cpu_max: i32,
+    logical_cpu: i32,
+    logical_cpu_max: i32,
+    max_mem: u64,
+}
+unsafe impl SafeRead for host_basic_info {}
+
+fn host_info(
+    env: &mut Environment,
+    _host: i32,
+    flavor: i32,
+    info_out: MutPtr<i32>,
+    info_cnt: MutPtr<u32>,
+) -> i32 {
+    match flavor {
+        HOST_BASIC_INFO => {
+            let mut info = host_basic_info::default();
+            
+            // Эмулируем железо iPad/iPhone
+            info.max_cpus = 1;
+            info.avail_cpus = 1;
+            info.physical_cpu = 1;
+            info.logical_cpu = 1;
+            
+            // CPU_TYPE_ARM = 12, CPU_SUBTYPE_ARM_V7 = 9
+            info.cpu_type = 12;
+            info.cpu_subtype = 9;
+            
+            // 256MB RAM (безопасное значение для старых игр)
+            let mem_bytes = 256 * 1024 * 1024;
+            info.memory_size = mem_bytes as u32;
+            info.max_mem = mem_bytes as u64;
+
+            let struct_size = (std::mem::size_of::<host_basic_info>() / 4) as u32;
+            let provided_cnt = env.mem.read(info_cnt);
+
+            if provided_cnt < struct_size {
+                return 4; // KERN_INVALID_ARGUMENT
+            }
+
+            env.mem.write(info_out.cast(), info);
+            env.mem.write(info_cnt, struct_size);
+            
+            0 // KERN_SUCCESS
+        }
+        _ => {
+            // Если запрашивают другой flavor, просто рапортуем об успехе без паники
+            0 
+        }
+    }
+}
+// -----------------------------------------------------
 
 fn get_end(env: &mut Environment) -> u32 {
     // Assume app binary is the first.
@@ -34,4 +101,8 @@ fn get_etext(env: &mut Environment) -> u32 {
     text_section.next_section_addr()
 }
 
-pub const FUNCTIONS: FunctionExports = &[export_c_func!(get_end()), export_c_func!(get_etext())];
+pub const FUNCTIONS: FunctionExports = &[
+    export_c_func!(get_end()), 
+    export_c_func!(get_etext()),
+    export_c_func!(host_info(_, _, _, _, _)),
+];
